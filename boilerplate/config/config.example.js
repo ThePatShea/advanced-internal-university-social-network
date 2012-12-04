2d7da890bae57256373b5b75fa55063cc657e628
Campus Bubble <development@thecampusbubble.com>
2012-12-04 09:41:48 -0500

    Made changes to boilerplate

---
+++ boilerplate/config/config.example.js
@@ -0,0 +1,32 @@
+
+module.exports = {
+    development: {
+      db: 'mongodb://localhost/noobjs_dev',
+      facebook: {
+          clientID: "219781404699497"
+        , clientSecret: "11efd0dea9edbb5461a0b74c6227ff42"
+        , callbackURL: "http://localhost:3000/auth/facebook/callback"
+      },
+      twitter: {
+          clientID: "CONSUMER_KEY"
+        , clientSecret: "CONSUMER_SECRET"
+        , callbackURL: "http://localhost:3000/auth/twitter/callback"
+      },
+      github: {
+          clientID: 'APP_ID'
+        , clientSecret: 'APP_SECRET'
+        , callbackURL: 'http://localhost:3000/auth/github/callback'
+      },
+      google: {
+          clientID: "APP_ID"
+        , clientSecret: "APP_SECRET"
+        , callbackURL: "http://localhost:3000/auth/google/callback"
+      }
+    }
+  , test: {
+
+    }
+  , production: {
+
+    }
+}
