36b2be9de44bc1568c557bd40242a53fee41bce8
Campus Bubble <development@thecampusbubble.com>
2012-12-11 02:18:38 -0500

    Temporarily made it so sorting events into bubbles doesn't just get future events

--- curate/javascripts/base_mongo.js
+++ curate/javascripts/base_mongo.js
@@ -180,7 +180,7 @@
 					var current_time = Math.round((new Date()).getTime() / 1000);
 					var yesterday = current_time - 86400;
 
-					mongo_model.find({$or : [{creator : {$in : page_array}}, {"venue.id" : {$in : page_array}}], privacy : "OPEN"}, "eid name pic_big start_time end_time location venue.id creator").where("end_time").gt(current_time).where("start_time").gt(yesterday).exec(function (err, mongo_model) {
+					mongo_model.find({$or : [{creator : {$in : page_array}}, {"venue.id" : {$in : page_array}}], privacy : "OPEN"}, "eid name pic_big start_time end_time location venue.id creator").exec(function (err, mongo_model) {
 						var event_array = mongo_model;
 						console.log(event_array);
 
