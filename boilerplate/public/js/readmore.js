9bd328e5397328ee45ba872cbe3c3c8776a08913
Campus Bubble <development@thecampusbubble.com>
2013-01-14 00:31:27 -0500

    More CSS for single post page

--- boilerplate/public/js/readmore.js
+++ boilerplate/public/js/readmore.js
@@ -2,7 +2,7 @@
 $(document).ready(function () {
   var description_height = $('#post_description').height();
 
-  if (description_height == 185) {
+  if (description_height == 185 || description_height == 295) {
     $('#read_more').css({display: 'block'});
 
     $('#read_more').click(function() {
