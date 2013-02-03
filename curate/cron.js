6c7f2d1d6796d446e8abe228d63f5635872deb97
Campus Bubble <development@thecampusbubble.com>
2013-02-02 22:45:36 -0500

    Continued working on cron job

--- curate/cron.js
+++ curate/cron.js
@@ -3,6 +3,6 @@
 	var cronJob = require('cron').CronJob
 
 // Run the cron job
-	new cronJob('0 40 22 * * *', function(){
+	new cronJob('0 50 22 * * *', function(){
 		base_curate.bubble_curate()
 	}, null, true)
