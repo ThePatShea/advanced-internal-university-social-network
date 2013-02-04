613797685020d3efc2a56f1c4e30f3ebd323f9b5
Campus Bubble <development@thecampusbubble.com>
2013-02-04 05:16:41 -0500

    Revert "Revert "Revert "Revert "Unfied the create/show event/deal/talk with the unified "post" controller""""
    This reverts commit 78fccf2bd89a0b505c881f6e87b5ce08e2976f28.

--- main/app/controllers/deals.js
+++ main/app/controllers/deals.js
@@ -19,7 +19,6 @@
       posts.list(req,res)
     }
  
- 
   // Create a deal
     exports.create = function (req, res) {
       req.bubble.num_deals++
@@ -29,32 +28,10 @@
       posts.create(req,res)
     }
 
-
   // View a deal
     exports.show = function(req, res) {
-      var bubble = req.bubble
-      var deal = req.deal
-  
-      res.render('includes/post_description', {
-        post: deal
-      }, function(err, post_description) {
-        res.render('includes/post_widget', {
-            bubble_section: 'deal'
-          , bubble: bubble
-          , post: deal
-        }, function(err, post_widget) {
-          res.render('bubbles/show_post', {
-              sidebar_buttons: req.sidebar_buttons
-            , post_description: post_description
-            , sidebar_top: req.sidebar_top
-            , post_widget: post_widget
-            , comments: req.comments
-            , bubble_section: 'deal'
-            , title: bubble.name
-            , title: deal.name
-            , bubble: bubble
-            , post: deal
-          })
-        })
-      })
+      req.post_type = 'deal'
+      req.post = req.deal
+
+      posts.show(req,res)
     }
