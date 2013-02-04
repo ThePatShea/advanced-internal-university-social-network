6647eb0298e65aabaeda8d64ca7565bc904a0035
Campus Bubble <development@thecampusbubble.com>
2013-02-04 08:56:55 -0500

    Got infinite scroll to grab from the correct bubble

--- main/public/js/infinitescrollandformatdate.js
+++ main/public/js/infinitescrollandformatdate.js
@@ -30,9 +30,11 @@ $(document).ready(function () {
 
   
   // Infinite scroll
-    var ajax_current  =  0;
-    var skip          =  20;
-    
+    var hidden_bubble_section  =  $('#hidden_bubble_section').html();
+    var hidden_bubble_id       =  $('#hidden_bubble_id').html();
+    var skip                   =  20;
+    var ajax_current           =  0;
+
     $(window).scroll(function() {
         if ( $(window).scrollTop() >= ( $(document).height() - $(window).height() - 1000) ) {
   
@@ -42,7 +44,7 @@ $(document).ready(function () {
               setTimeout(function() { ajax_current = 0; }, 1000);
       
             // Load the new data
-              $.get('/bubbles/510f16f12898e00238000005/events_list_pagelet/'+skip, function(data) {
+              $.get('/bubbles/'+hidden_bubble_id+'/'+hidden_bubble_section+'s_list_pagelet/'+skip, function(data) {
                 $('#post_list').append(data);
   
                 format_date('format_date_bottom', 'h:mma', skip);
