75872cde5443c0409a177436a8177ecce8c2fa89
Campus Bubble <development@thecampusbubble.com>
2013-02-04 07:12:56 -0500

    Got the jquery/aja part of infinite scroll working. Still need to make it dynamic.

--- main/public/js/infinitescroll-ajax.js
+++ main/public/js/infinitescroll-ajax.js
@@ -2,26 +2,22 @@
 $(document).ready(function () {
 
   var ajax_current = 0;
-  var ajax;
   
   $(window).scroll(function() {
       if ( $(window).scrollTop() >= ( $(document).height() - $(window).height() - 1000) ) {
-          ajax = ajaxLoadActivity();
+
+        if (ajax_current == 0) {
+          // Prevent it from loading too much data
+            ajax_current = 1;
+            setTimeout(function() { ajax_current = 0; }, 1000);
+    
+          // Load the new data
+            $.get('/bubbles/510f16f12898e00238000005/events_list_pagelet', function(data) {
+              $('#post_list').append(data);
+            });
+        }
+
       }
   });
-  
-  function ajaxLoadActivity() {
-    if (ajax_current == 0) {
-      ajax_current = 1;
-
-      setTimeout(function(){
-        ajax_current = 0;
-      },1000);
-
-      return $.get('/bubbles/510f16f12898e00238000005/events_list_pagelet', function(data) {
-        $('#post_list').append(data);
-      });
-    }
-  }
 
 });
