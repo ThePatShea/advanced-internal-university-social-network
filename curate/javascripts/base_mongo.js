ca0bfded6ce273ff5828aba13399186001064128
Campus Bubble <development@thecampusbubble.com>
2013-02-08 05:00:38 -0500

    Closed some files

--- curate/javascripts/base_mongo.js
+++ curate/javascripts/base_mongo.js
@@ -4,6 +4,9 @@
 	else
 		var db_connect = 'mongodb://nodejitsu_campusbubble:mm0hjn9lob87vt9eopnpshp13b@ds049537.mongolab.com:49537/nodejitsu_campusbubble_nodejitsudb4086692456';   // Development database
 
+	// TESTING
+		// var db_connect = 'mongodb://nodejitsu_campusbubble:vn94subvihmm5j843t4to71s5g@ds049537.mongolab.com:49537/nodejitsu_campusbubble_nodejitsudb9203155674';   // Production database
+	// TESTING
 
 	var mongoose  =  require('mongoose')
           , db        =  mongoose.createConnection(db_connect)
