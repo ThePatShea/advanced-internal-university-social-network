5d2a65ba9ef5df77aef437875546adbaf845993c
Campus Bubble <development@thecampusbubble.com>
2013-02-11 22:54:22 -0500

    Richt before trying to implement uploading images into the main server

--- sandbox/rackit_test/rackit_test.js
+++ sandbox/rackit_test/rackit_test.js
@@ -1,16 +1,10 @@
 var rackit = require('rackit');
 
-// Initialize with your account information
 rackit.init({
     'user' : 'campusbubble',
     'key' : 'f12ab1992b6f9252fcce6be07091afd5'
 }, function(err) {
-    // Add a local file to the cloud
-    rackit.add(__dirname + '/image.jpg', function(err, cloudpath) {
-        // Get the CDN URI of the file
+    rackit.add(__dirname + '/image2.png', function(err, cloudpath) {
         console.log(rackit.getURI(cloudpath));
-
-        // The cloudpath parameter uniquely identifies this file, and is used by other Rackit methods to manipulate it.
-        // We should probably store the cloudpath somewhere.
     });
 });
