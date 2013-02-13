9c9fc0aa1e403cfbc58d52ae98444bce59424d9a
Campus Bubble <development@thecampusbubble.com>
2013-02-12 19:26:46 -0500

    More template progress

--- main/authorization.js
+++ main/authorization.js
@@ -47,21 +47,6 @@ exports.post = {
 
         next()
       }
-    , authorized_widgets : function (req, res, next) {
-        if (req.post.creator == req.user.id) {
-          res.render('posts/change_post_image', {
-              bubble_section: req.bubble_section
-            , bubble: req.bubble
-            , post: req.post
-          }, function(err, change_post_image) {
-            req.change_post_image = change_post_image
-            next()
-          })
-        } else {
-            req.change_post_image = ''
-            next()
-        }
-      }
 }
 
 
