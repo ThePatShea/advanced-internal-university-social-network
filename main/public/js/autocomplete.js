b8a69d3cc6ebe5936b2f9aa6a6eacb13c5bee0d8
Campus Bubble <development@thecampusbubble.com>
2013-03-26 23:57:52 -0400

    More autocomplete

--- main/public/js/autocomplete.js
+++ main/public/js/autocomplete.js
@@ -1,4 +1,44 @@
 // Controls the autocomplete box that grabs information from the database
 $(document).ready(function () {
-  alert("autocomplete them jetskis!");
+
+  $('#invite_member').click(function() {
+    $('#invite_member_search').css({display: 'block'});
+    $('#invite_member_start').css({display: 'none'});
+  });
+
+$(function() {
+  var availableTags = [
+      "ActionScript",
+      "AppleScript",
+      "Asp",
+      "BASIC",
+      "C",
+      "C++",
+      "Clojure",
+      "COBOL",
+      "ColdFusion",
+      "Erlang",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala",
+      "Scheme"
+    ];
+
+    $('#invite_member_input').autocomplete({
+      source: availableTags,
+      minLength: 2,
+      select: function( event, ui ) {
+        alert('clicked!')
+      }
+    });
+})
+
 })
