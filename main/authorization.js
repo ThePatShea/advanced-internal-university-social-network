803ee4b271fa99096130ccd47342826e06dea96f
Campus Bubble <development@thecampusbubble.com>
2013-03-27 23:07:34 -0400

    More views/permissions

--- main/authorization.js
+++ main/authorization.js
@@ -1,23 +1,3 @@
-// Authorize sign in
-  exports.requiresLogin = function (req, res, next) {
-    if (!req.isAuthenticated())
-      return res.redirect('/login')
-
-    next()
-  };
-
-
-// Authorize user
-  exports.user = {
-      hasAuthorization: function (req, res, next) {
-        if (req.profile.id != req.user.id)
-          return res.redirect('/users/'+req.profile.id)
-
-        next()
-      }
-  }
-
-
 // Authorize post
   exports.post = {
       hasAuthorization: function (req, res, next) {
@@ -64,3 +44,23 @@
         next()
       }
 }
+
+
+// Authorize user
+  exports.user = {
+      hasAuthorization: function (req, res, next) {
+        if (req.profile.id != req.user.id)
+          return res.redirect('/users/'+req.profile.id)
+
+        next()
+      }
+  }
+
+
+// Authorize sign in
+  exports.requiresLogin = function (req, res, next) {
+    if (!req.isAuthenticated())
+      return res.redirect('/login')
+
+    next()
+  };
