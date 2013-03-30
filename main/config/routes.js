0c4e87554b4abb8f63735875cbed02bce5023437
Campus Bubble <development@thecampusbubble.com>
2013-03-30 00:20:45 -0400

    Finished views/permissions

--- main/config/routes.js
+++ main/config/routes.js
@@ -125,7 +125,7 @@ module.exports = function (app, passport, auth) {
 
   // Post Routes
     // Get Requests
-      app.get('/bubbles/:bubbleId/:bubble_section/view/:postId'        ,  auth.requiresLogin  ,  auth.bubble.get_connect_status  ,  auth.post.get_connect_status  ,  auth.bubble.redirect_member_if_private          ,  sidebar.bubble  ,  posts.single)
+      app.get('/bubbles/:bubbleId/:bubble_section/view/:postId'        ,  auth.requiresLogin  ,  auth.bubble.get_connect_status  ,  auth.post.get_connect_status  ,  auth.post.redirect_member_if_private          ,  sidebar.bubble  ,  posts.single)
       app.get('/bubbles/:bubbleId/:bubble_section/edit/:postId'        ,  auth.requiresLogin  ,  auth.bubble.get_connect_status  ,  auth.post.get_connect_status  ,  auth.post.redirect_creator_or_admin  ,  sidebar.bubble  ,  posts.edit)
       app.get('/bubbles/:bubbleId'                                     ,  auth.requiresLogin  ,  auth.bubble.get_connect_status  ,  sidebar.bubble                ,  posts.dashboard)
       app.get('/bubbles/:bubbleId/:bubble_section'                     ,  auth.requiresLogin  ,  auth.bubble.get_connect_status  ,  sidebar.bubble                ,  posts.list)
