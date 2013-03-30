d2272cf7a32eecc6ab1a7086899cc4453068fa57
Campus Bubble <development@thecampusbubble.com>
2013-03-30 00:08:05 -0400

    More views/permissions

--- main/authorization.js
+++ main/authorization.js
@@ -3,6 +3,8 @@
       get_connect_status: function (req, res, next) {
              if (req.user.connections.bubbles.admin.indexOf(req.bubble.id) > -1)
           req.post_connect_status  =  'admin'
+        else if (req.user.connections.bubbles.member.indexOf(req.bubble.id) > -1)
+          req.post_connect_status  =  'member'
         else if (req.post.creator == req.user.id)
           req.post_connect_status  =  'creator'
         else 
@@ -11,10 +13,18 @@
         next()
       }
     , redirect_creator_or_admin: function (req, res, next) {
-        var post_connect_status    =  req.post_connect_status
+        var post_connect_status  =  req.post_connect_status
 
         if (post_connect_status != 'creator' && post_connect_status != 'admin')
           return res.redirect('/bubbles/' + req.bubble._id + '/' + req.bubble_section + '/view/' + req.post._id)
+        
+        next()
+      }
+    , redirect_member_if_private: function (req, res, next) {
+        var post_connect_status  =  req.post_connect_status
+
+        if (req.post.privacy == 'private' && post_connect_status != 'member' && post_connect_status != 'admin')
+          return res.redirect('/bubbles/' + req.bubble._id)
         
         next()
       }
