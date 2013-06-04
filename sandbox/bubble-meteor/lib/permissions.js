06267d2125d916bd1c94b7d8b66272ecfbc0cc00
Junhao <haopjh@gmail.com>
2013-06-05 07:00:12 +0800

    removed old meteor project

--- sandbox/bubble-meteor/lib/permissions.js
+++
@@ -1,9 +0,0 @@
-// check that the userId specified owns the documents
-ownsDocument = function(userId, doc) {
-	var user = Meteor.users.findOne({_id: Meteor.user()._id});
-	if (user.username == 'admin'){
-		return doc && true;
-	}else{
-  	return doc && doc.userId === userId;
-	}
-}
