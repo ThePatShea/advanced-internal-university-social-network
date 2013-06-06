95c81fe5712d791e0369950cbcefbd781f24fd4a
Junhao <haopjh@gmail.com>
2013-06-07 03:45:48 +0800

    Removed cruft boilerplates

--- sandbox/meteor_test_pat/microscope2/client/main.js
+++
@@ -1,8 +0,0 @@
-Meteor.subscribeWithPagination('posts', 10);
-Meteor.subscribe('comments');
-
-Deps.autorun(function() {
-  Meteor.subscribe('comments', Session.get('currentPostId'));
-});
-
-Meteor.subscribe('notifications');
