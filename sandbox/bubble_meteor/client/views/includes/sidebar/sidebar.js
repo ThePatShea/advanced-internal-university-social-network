9fd11b964c7435d36374c5308d3162ba4dc45b5b
Junhao <haopjh@gmail.com>
2013-06-08 04:13:43 +0800

    Added notification badges to bubbles in sidebar

--- sandbox/bubble_meteor/client/views/includes/sidebar/sidebar.js
+++ sandbox/bubble_meteor/client/views/includes/sidebar/sidebar.js
@@ -1,5 +1,9 @@
 Template.sidebar.helpers({
-  bubbles: function(){
-    return Bubbles.find();
+  notificationCount: function(){
+  	return Notifications.find({userId: Meteor.userId(), read: false, bubbleId: this._id}).count();
+  },
+  activeNotificationClass: function() {
+  	var count = Notifications.find({userId: Meteor.userId(), read: false, bubbleId: this._id}).count();
+    return (count > 0) && 'active';
   }
 });
