2efd42d343fbcc66b39329d6efcd307de95435d1
Junhao <haopjh@gmail.com>
2013-06-13 01:25:02 +0800

    Added mail_url for udpates

--- sandbox/bubble_meteor/client/main.js
+++ sandbox/bubble_meteor/client/main.js
@@ -9,6 +9,6 @@ Deps.autorun(function() {
 Meteor.subscribe('updates');
 Meteor.subscribe('bubbles');
 
-// Meteor.startup(function () {
-//   process.env.MAIL_URL = 'smtp://postmaster%40meteorize.mailgun.org:YOURPASSWORD@smtp.mailgun.org:587';
-// });
+Meteor.startup(function () {
+  // process.env.MAIL_URL = 'smtp://postmaster%40meteorize.mailgun.org:YOURPASSWORD@smtp.mailgun.org:587';
+});
