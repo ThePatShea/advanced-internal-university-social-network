46fd903b8f91c39ea1020b69336866dbd4c19ad5
fernandes.praphat@gmail.com <fernandes.praphat@gmail.com>
2013-06-19 19:23:10 -0400

    Image re-sizing in the WYSIWYG editor only kicks in if the image is too large. It leaves small images untouched.

--- sandbox/bubble_meteor/client/views/includes/widgets/wysiwyg/wysiwyg.js
+++ sandbox/bubble_meteor/client/views/includes/widgets/wysiwyg/wysiwyg.js
@@ -6,6 +6,8 @@ Template.wysiwyg.rendered = function() {
   dom_modified_parseimages looks for unparsed images, changes their width
   and height, and then marks them as parsed.
   */
+
+
   dom_modified_findimages = function(e){
   	images = $(e.srcElement).find('img');
   	for(var i = 0; i < images.length; i++){
@@ -23,8 +25,21 @@ Template.wysiwyg.rendered = function() {
   		var image = images[i];
   		if($(image).attr('class') != 'parsed'){
   			$(image).attr('class', 'parsed');
-	  		$(image).attr('width', 300);
-	  		$(image).attr('height', 300);
+
+  			//The height and width of each image is checked, and if either
+  			//are greater than 300, then the image height and width attributes
+  			//are reset to 300. This keeps small images untouched, while
+  			//shrinking larger images. Aspect ratio is preserved.
+    		$(image).load(function(){
+		  		width = $(image).width();
+		  		height = $(image).height();
+		  		console.log(width, height);
+		  		if(width > 300 || height > 300){
+			  		$(image).attr('width', 300);
+			  		$(image).attr('height', 300);	
+		  		}
+		  	});
+
   		}
   	}
   };
