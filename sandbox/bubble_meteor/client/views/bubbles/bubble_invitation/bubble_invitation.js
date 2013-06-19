50cf0914fb11e5c25a13c7ec235bfcaf5fba00d9
shaoqi <qi.shao.2010@sis.smu.edu.sg>
2013-06-19 10:45:54 -0400

    merge to pair program with junhao

--- sandbox/bubble_meteor/client/views/bubbles/bubble_invitation/bubble_invitation.js
+++ sandbox/bubble_meteor/client/views/bubbles/bubble_invitation/bubble_invitation.js
@@ -1,42 +1,20 @@
 Template.bubbleInvitation.events({
   'submit form': function(e) {
     e.preventDefault();
-		
-		//console.log(this.searchContent); //the searchContent is not saved yet
-		//but the content is stored into session
-		//how to store it????
-		Session.set('selectedUsername', $(e.target).find('[name=title]').val());
-  },
+    //store the content into the session
+		Session.set('selectedUsername', $(e.target).find('[name=title]').val()); 
+    Session.set('currentUserId', Meteor.userId());
+  },	
 });
 
 Template.bubbleInvitation.helpers({
-
-	findUsers: function(username){
-	  //var users = Meteor.users.find({username:username});
-	  return Session.get('selectedUsername'); //all the users are found
-	},
-	
-	getSearchText: function(){
-		console.log(this.searchContent);
-		return this.searchContent;
-	},
-	
-	getSelectedUsers: function(){
-		//return Meteor.users.find({username:Session.get('selectedUsername')});
-		//Meteor.users.find({username:Session.get('selectedUsername')});
-		
-		//console.log(Meteor.users.find({username:Session.get('selectedUsername')}).fetch());
-		return Meteor.users.find();
+	findUsers: function(){
+	  return Meteor.users.find();
 	}
-
 });
 
-Template.selectedUserItem.helpers({
-	
-	getSelectedUser: function(){
-		return "Andy"; 
-	}
-})
+
+
 
 
 
