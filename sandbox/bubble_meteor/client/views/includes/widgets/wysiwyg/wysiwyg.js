5f9ee44d7a80c984d84fb86a5458e180d2125131
fernandes.praphat@gmail.com <fernandes.praphat@gmail.com>
2013-06-19 18:34:45 -0400

    Got basic auto-image re-sizing in the WYSIWYG editor to work. This now needs to be implemented by using the HTML5 Canvas to actually re-size the images.

--- sandbox/bubble_meteor/client/views/includes/widgets/wysiwyg/wysiwyg.js
+++ sandbox/bubble_meteor/client/views/includes/widgets/wysiwyg/wysiwyg.js
@@ -1,3 +1,34 @@
 Template.wysiwyg.rendered = function() {
   $('.wysiwyg').wysiwyg();
+
+  /*Two handlers are bound to the DOMSubtreeModified event
+  dom_modified_findimages looks for new images and marks them as unparsed.
+  dom_modified_parseimages looks for unparsed images, changes their width
+  and height, and then marks them as parsed.
+  */
+  dom_modified_findimages = function(e){
+  	images = $(e.srcElement).find('img');
+  	for(var i = 0; i < images.length; i++){
+  		var image = images[i];
+  		if($(image).attr('class') != 'parsed'){
+  			$(image).attr('class', 'unparsed');
+  		}
+  	}
+  };
+
+  dom_modified_parseimages = function(e){
+  	//console.log(e);
+  	images = $(e.srcElement).find('img');
+  	for(var i = 0; i < images.length; i++){
+  		var image = images[i];
+  		if($(image).attr('class') != 'parsed'){
+  			$(image).attr('class', 'parsed');
+	  		$(image).attr('width', 300);
+	  		$(image).attr('height', 300);
+  		}
+  	}
+  };
+
+  $('.wysiwyg').bind('DOMSubtreeModified', dom_modified_findimages);
+  $('.wysiwyg').bind('DOMSubtreeModified', dom_modified_parseimages);
 }
