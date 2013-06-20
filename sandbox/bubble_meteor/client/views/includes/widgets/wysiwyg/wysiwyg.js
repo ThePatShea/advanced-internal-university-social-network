0e6fdc0d5983e329ec9d5d55ae2ddd8819b11bdc
fernandes.praphat@gmail.com <fernandes.praphat@gmail.com>
2013-06-19 20:44:17 -0400

    Auto image re-sizing in the WYSIWYG editor works, and is now carried out using the HTML5 canvas, so that the images are actually re-sized. Thus the images which are saved into the database are actually smaller in size.

--- sandbox/bubble_meteor/client/views/includes/widgets/wysiwyg/wysiwyg.js
+++ sandbox/bubble_meteor/client/views/includes/widgets/wysiwyg/wysiwyg.js
@@ -1,6 +1,9 @@
 Template.wysiwyg.rendered = function() {
   $('.wysiwyg').wysiwyg();
 
+  var maxwidth = 300;
+  var maxheight = 300;
+
   /*Two handlers are bound to the DOMSubtreeModified event
   dom_modified_findimages looks for new images and marks them as unparsed.
   dom_modified_parseimages looks for unparsed images, changes their width
@@ -27,16 +30,31 @@ Template.wysiwyg.rendered = function() {
   			$(image).attr('class', 'parsed');
 
   			//The height and width of each image is checked, and if either
-  			//are greater than 300, then the image height and width attributes
-  			//are reset to 300. This keeps small images untouched, while
-  			//shrinking larger images. Aspect ratio is preserved.
+  			//are greater than the maxwidth or maxheight, then the image is
+  			//drawn on an HTML5 canvas element and re-scaled. This keeps small
+  			//images untouched, while shrinking larger images.
+  			//Aspect ratio is preserved.
     		$(image).load(function(){
-		  		width = $(image).width();
-		  		height = $(image).height();
+    			var mycanvas = document.createElement('canvas');
+		  		width = $(this).width();
+		  		height = $(this).height();
+		  		var ratio = 1;
+		  		if(width > maxwidth){
+		  			ratio = maxwidth / width;
+		  		}
+		  		else if(height > maxheight){
+		  			ratio = maxheight / height;
+		  		}
+
+		  		mycanvas.width = width*ratio;
+		  		mycanvas.height = height*ratio;
+
 		  		console.log(width, height);
-		  		if(width > 300 || height > 300){
-			  		$(image).attr('width', 300);
-			  		$(image).attr('height', 300);	
+		  		if(width > maxwidth || height > maxheight){
+			  		var context = mycanvas.getContext('2d');
+			  		context.drawImage(this, 0, 0, mycanvas.width, mycanvas.height);
+			  		var imagedata = mycanvas.toDataURL();
+			  		$(this).attr("src", imagedata);
 		  		}
 		  	});
 
