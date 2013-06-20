f6eec447b013883dbdb7b10e16a4b9b6bcefe1f1
fernandes.praphat@gmail.com <fernandes.praphat@gmail.com>
2013-06-20 17:29:40 -0400

    Users may now select a title image when creating a bubble. This image is now shown on the bubble cover page.

--- sandbox/bubble_meteor/client/views/bubbles/bubble_submit/bubble_submit.js
+++ sandbox/bubble_meteor/client/views/bubbles/bubble_submit/bubble_submit.js
@@ -5,7 +5,8 @@ Template.bubbleSubmit.events({
     var bubble = {
       title: $(event.target).find('[name=title]').val(),
       description: $(event.target).find('[name=description]').val(),
-      category: $(event.target).find('[name=category]').val()
+      category: $(event.target).find('[name=category]').val(),
+      titleImage: $(event.target).find('[id=bubblepic]').attr('src')
     }
     
     Meteor.call('bubble', bubble, function(error, bubbleId) {
@@ -16,5 +17,58 @@ Template.bubbleSubmit.events({
         Meteor.Router.to('bubblePage', bubbleId);
       }
     });
-  }
+  },
+
+
+    'dragover #drop_zone': function(evt){
+    console.log('Dragover');
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt.dataTransfer.dropEffect = 'copy';
+  },
+
+  'drop #drop_zone': function(evt){
+    console.log('Drop');
+        evt.stopPropagation();
+    evt.preventDefault();
+
+    files = evt.dataTransfer.files;
+    if(files.length > 1){
+      error = new Meteor.Error(422, 'Please choose only one image as the bubble image.');
+      throwError(error.reason);
+    }
+    else{
+      f = files[0];
+      //If it is an image then render a thumbnail
+      if (f.type.match('image.*')) {
+        var reader = new FileReader();
+
+        // Closure to capture the file information.
+        reader.onload = (function(theFile) {
+        return function(e) {
+        // Render thumbnail.
+        dropzone = document.getElementById('drop_zone');
+        parent = document.getElementById('bubble_image');
+        parent.removeChild(dropzone);
+        var span = document.createElement('span');
+        span.innerHTML = ['<img id="bubblepic" class="thumb" src="', e.target.result,
+                        '" title="', escape(theFile.name), '"/>'].join('');
+        document.getElementById('bubble_image').appendChild(span);
+        };
+        })(f);
+
+        // Read in the image file as a data URL.
+        reader.readAsDataURL(f);
+      }
+
+      else{
+        error = new Meteor.Error(422, 'Please choose a valid image.');
+        throwError(error.reason);
+      }
+    }
+
+
+    
+  },
+
 });
