366ebf2ddaf4626debbdeb82409417ade6f50217
fernandes.praphat@gmail.com <fernandes.praphat@gmail.com>
2013-06-24 16:34:17 -0400

    Added functionality to upload a photo for an event. Only single image uploads are accepted, and only images are accepted. The user is asked to select a cropped portion of the image, which is stored as the event photo. This is then displayed on the event page.

--- sandbox/bubble_meteor/client/views/posts/post_submit/event_submit/event_submit.js
+++ sandbox/bubble_meteor/client/views/posts/post_submit/event_submit/event_submit.js
@@ -11,9 +11,90 @@ Template.eventSubmit.events({
       body: $(event.target).find('[name=body]').val(),
       postType: 'event',
       bubbleId: Session.get('currentBubbleId'),
-      attendees: [Meteor.users.findOne().username]
+      attendees: [Meteor.users.findOne().username],
+      eventPhoto: $(event.target).find('[id=eventphoto_preview]').attr('src')
     });
-  }
+  },
+
+    'dragover .dropzone': function(evt){
+    console.log('Dragover');
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt.dataTransfer.dropEffect = 'copy';
+  },
+
+  'drop .dropzone': function(evt){
+    console.log('Drop');
+        evt.stopPropagation();
+    evt.preventDefault();
+
+    files = evt.dataTransfer.files;
+
+    //If more than one file dropped on the dropzone then throw an error to the user.
+    if(files.length > 1){
+      error = new Meteor.Error(422, 'Please choose only one image as the bubble image.');
+      throwError(error.reason);
+    }
+    else{
+      f = files[0];
+      //If the file dropped on the dropzone is an image then start processing it
+      if (f.type.match('image.*')) {
+        var reader = new FileReader();
+
+        // Closure to capture the file information.
+        reader.onload = (function(theFile) {
+          return function(e) {
+            var coverphoto_width = 380;
+            var coverphoto_height = 240;
+            $("#eventphoto_dropzone").hide();
+            $("#eventphoto_upload").attr("src", e.target.result);
+            $("#eventphoto_preview").attr("src", e.target.result);
+            $("#eventphoto_upload").attr("title", escape(theFile.name));
+            $("#eventphoto_upload").show();
+            $(document).ready( function(){
+              $(function(){
+                function showPreview(coords){
+                  var mycanvas = document.createElement('canvas');
+                  mycanvas.width = coverphoto_width;
+                  mycanvas.height = coverphoto_height;
+                  console.log(coords);
+                  mycontext = mycanvas.getContext('2d');
+                  mycontext.drawImage($("#eventphoto_upload")[0], coords.x, coords.y, (coords.x2 - coords.x), (coords.y2 - coords.y), 0, 0, coverphoto_width, coverphoto_height);
+                  var imagedata = mycanvas.toDataURL();
+                  $("#eventphoto_preview").attr("src", imagedata);
+                  $("#eventphoto_preview").attr("width", coverphoto_width/2);
+                  $("#eventphoto_preview").attr("height", coverphoto_height/2);
+                };
+
+                $('#eventphoto_upload').Jcrop({
+                  onChange: showPreview,
+                  onSelect: showPreview,
+                  setSelect:   [ 50, 50, coverphoto_width, coverphoto_height ],
+                  aspectRatio: coverphoto_width/coverphoto_height
+                }, function(){
+                  jcrop_api = this;
+                  jcrop_api.setOptions({ allowResize: false });
+                });
+
+              });
+            });
+          };
+        })(f);
+
+        // Read in the image file as a data URL.
+        reader.readAsDataURL(f);
+
+      }
+
+      //If the file dropped on the dropzone is not an image then throw an error to the user
+      else{
+        error = new Meteor.Error(422, 'Please choose a valid image.');
+        throwError(error.reason);
+      }
+    }
+
+  },
+
 });
 
 Template.eventSubmit.rendered = function() {
