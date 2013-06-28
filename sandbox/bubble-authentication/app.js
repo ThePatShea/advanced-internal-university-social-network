3c8f522ffd86364d8120b532ce4608a5a5b28555
Praphat Xavier Fernandes <fernandes.praphat@gmail.com>
2013-06-28 20:23:03 +0000

    The bubble-authentication node.js server works with Feide OpenIDP. This commit is after it has been modified to be tested against Shibboleth for Emory. There are bugs in passport-saml in relation to Shibboleth and that code needs to be updated.

--- sandbox/bubble-authentication/app.js
+++ sandbox/bubble-authentication/app.js
@@ -1,4 +1,5 @@
 
+
 /**
  * Module dependencies.
  */
@@ -12,41 +13,37 @@ var express = require('express')
   , BasicStrategy = require('passport-http').BasicStrategy
   , SamlStrategy = require('passport-saml').Strategy
   , mongoose = require('mongoose')
-  , crypto = require('crypto')
-  , querystring = require('querystring');
+  , fs = require('fs');
 
 mongoose.connect('mongodb://localhost:27017/test');
+
+var globalprofile = '';
+//var date = new Date();
 
 var userSchema = mongoose.Schema({
   username: String,
   email: String
-});
-
+})
 
 var User = mongoose.model('User',userSchema);
 
-
-User.find({},
-  function(err, users){
-    if(err) console.log(err);
-    if(users.length == 0){
-      var newuser = User({username: 'pxferna', email: 'fernandes.praphat@gmail.com'});
-      newuser.save();
-    }
-  });
-
-
 passport.use(new SamlStrategy(
   {
-    path: '/login/callback',
-    entryPoint: 'https://openidp.feide.no/simplesaml/saml2/idp/SSOService.php',
-    issuer: 'passport-saml'
+    path: '/login/samlcallback',
+    //entryPoint: 'https://openidp.feide.no/simplesaml/saml2/idp/SSOService.php',
+    entryPoint: 'https://idp.guest.vm.org/idp/profile/SAML2/Redirect/SSO',
+    issuer: 'emorybubble',
+    //cert: 'MIICizCCAfQCCQCY8tKaMc0BMjANBgkqhkiG9w0BAQUFADCBiTELMAkGA1UEBhMCTk8xEjAQBgNVBAgTCVRyb25kaGVpbTEQMA4GA1UEChMHVU5JTkVUVDEOMAwGA1UECxMFRmVpZGUxGTAXBgNVBAMTEG9wZW5pZHAuZmVpZGUubm8xKTAnBgkqhkiG9w0BCQEWGmFuZHJlYXMuc29sYmVyZ0B1bmluZXR0Lm5vMB4XDTA4MDUwODA5MjI0OFoXDTM1MDkyMzA5MjI0OFowgYkxCzAJBgNVBAYTAk5PMRIwEAYDVQQIEwlUcm9uZGhlaW0xEDAOBgNVBAoTB1VOSU5FVFQxDjAMBgNVBAsTBUZlaWRlMRkwFwYDVQQDExBvcGVuaWRwLmZlaWRlLm5vMSkwJwYJKoZIhvcNAQkBFhphbmRyZWFzLnNvbGJlcmdAdW5pbmV0dC5ubzCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAt8jLoqI1VTlxAZ2axiDIThWcAOXdu8KkVUWaN/SooO9O0QQ7KRUjSGKN9JK65AFRDXQkWPAu4HlnO4noYlFSLnYyDxI66LCr71x4lgFJjqLeAvB/GqBqFfIZ3YK/NrhnUqFwZu63nLrZjcUZxNaPjOOSRSDaXpv1kb5k3jOiSGECAwEAATANBgkqhkiG9w0BAQUFAAOBgQBQYj4cAafWaYfjBU2zi1ElwStIaJ5nyp/s/8B8SAPK2T79McMyccP3wSW13LHkmM1jwKe3ACFXBvqGQN0IbcH49hu0FKhYFM/GPDJcIHFBsiyMBXChpye9vBaTNEBCtU3KjjyG0hRT2mAQ9h+bkPmOvlEo/aH0xR68Z9hw4PF13w==',
+    //privateCert: fs.readFileSync('/home/pxferna/certs/sp-key.pem', 'utf-8')
   },
-
   function(profile, done) {
-    User.findOne({'username': profile.uid}, function(err, user){
-      console.log(user);
-      return done(null, user);
+    globalprofile = JSON.stringify(profile);
+    User.findOne({username: profile.uid}, function (err, user) {
+      if (user == null){
+        user = new User({username: profile.uid, email:profile.email});
+        user.save();
+      }
+      return done(null,user);
     });
   }
 ));
@@ -64,7 +61,7 @@ passport.deserializeUser(function(id, done) {
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(passport.initialize());
@@ -81,57 +78,33 @@ app.get('/fail', function(req, res){
   res.render('fail');
 });
 
+app.get('/welcome', function(req, res){
+  var date = new Date();
+  res.render('welcome', {datetime: JSON.stringify(date)});
+});
+
 app.get('/login',
-  passport.authenticate('saml', { failureRedirect: '/', failureFlash: true }),
+  passport.authenticate('saml', { failureRedirect: '/fail', failureFlash: true }),
   function(req, res) {
     res.redirect('/home');
   }
 );
 
-app.post('/login/callback',
-
-  passport.authenticate('saml', { failureRedirect: '/', failureFlash: true }),
-
+app.post('/login/samlcallback',
+  passport.authenticate('saml', { failureRedirect: '/fail', failureFlash: true }),
   function(req, res) {
-    var seed = crypto.randomBytes(20);
-    var secret = crypto.createHash('sha1').update(seed).digest('hex');
-    // Build the post string from an object
-    //secret = '121212121212121212121212'
-    var post_data = querystring.stringify({
-      'username': req.user.username,
-      'secret':secret
-    });
-
-    var options = {
-      hostname: '127.0.0.1',
-      port: 8000,
-      path: '/usersecret',
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Content-Length': post_data.length
-      }
-    };
-    // Set up the request
-    var post_req = http.request(options, function(res) {
-      res.setEncoding('utf8');
-
-      // res.redirect(res.headers.location);
-      res.on('data', function (chunk) {
-          console.log('Response: ' + chunk);
-      });
-    });
-
-    // post the data
-    post_req.write(post_data);
-    post_req.end();
-    res.redirect('http://127.0.0.1:8000/user/' + req.user.username + '/' + secret);
+    //res.render('home', {username: req.user.username});
+    res.render('home', {username: globalprofile});
   }
-  
 );
 
 app.get('/home', function(req, res){
   res.render('home');
+});
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
 });
 
 // development only
@@ -140,6 +113,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
+
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
