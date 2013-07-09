9a5407db536d2e9aa16547fc9f39480ecda8b66e
Pat Shea <development@thecampusbubble.com>
2013-07-09 17:52:45 -0400

    got the member icon to turn blue in event_item if the user is attending the event

--- sandbox/bubble_meteor/client/views/posts/post_item/event_item/event_item.js
+++ sandbox/bubble_meteor/client/views/posts/post_item/event_item/event_item.js
@@ -1,5 +1,12 @@
+Template.eventItem.helpers({
+    isGoing : function() {
+      return _.contains(this.attendees,Meteor.user().username)
+    }
+});
+
+
 Template.eventItem.events({
-  'click .post-item': function() {
-    Meteor.Router.to('postPage', this.bubbleId, this._id);
-  }
+    'click .post-item' : function() {
+      Meteor.Router.to('postPage', this.bubbleId, this._id);
+    }
 });
