c2c50f38719e94e41ac000974fb3fb9ada7353da
fernandes.praphat@gmail.com <fernandes.praphat@gmail.com>
2013-07-15 13:24:02 -0400

    Crop tool for editing the userprofile now works.

--- sandbox/bubble_meteor/client/views/users/user_profile/edit_profile.js
+++ sandbox/bubble_meteor/client/views/users/user_profile/edit_profile.js
@@ -80,7 +80,7 @@ Template.userProfileEdit.events({
         var reader = new FileReader();
 
         // Closure to capture the file information.
-        reader.onload = (function(theFile) {
+        /*reader.onload = (function(theFile) {
           return function(e) {
             var coverphoto_width = 160;
             var coverphoto_height = 160;
@@ -126,6 +126,87 @@ Template.userProfileEdit.events({
 
               });
             });
+          };
+        })(f);*/
+
+        // Closure to capture the file information.
+        reader.onload = (function(theFile) {
+          return function(e) {
+            var coverphoto_width = 160;
+            var coverphoto_height = 160;
+            var retina_width = 320;
+            var retina_height = 320;
+            $("#userprofilepicture_dropzone").hide();
+            $("#userprofilepicture_upload").attr("src", e.target.result);
+            $("#userprofilepicture_preview").attr("src", e.target.result);
+            $("#userprofilepicture_upload").attr("title", escape(theFile.name));
+            $("#userprofilepicture_upload").show();
+            $("#userprofilepicture_preview").show();
+            $(document).ready(function(){
+              // Apply jrac on some image.
+              var tempcanvas = document.createElement('canvas');
+              var tempcontext = tempcanvas.getContext('2d');
+              var mycanvas = document.createElement('canvas');
+              var retinacanvas = document.createElement('canvas');
+              var mycontext = mycanvas.getContext('2d');
+              var retinacontext = retinacanvas.getContext('2d');
+              mycanvas.width = coverphoto_width;
+              mycanvas.height = coverphoto_height;
+              retinacanvas.width = retina_width;
+              retinacanvas.height = retina_height;
+              var cropx, cropy, cropwidth, cropheight, imagewidth, imageheight;
+              cropwidth = 150;
+              cropheight = 150;
+              var img = $("#userprofilepicture_upload")[0]; // Get my img elem
+              var pic_real_width, pic_real_height;
+              $("<img/>") // Make in memory copy of image to avoid css issues
+                  .attr("src", $(img).attr("src"))
+                  .load(function() {
+                      imagewidth = this.width;   // Note: $(this).width() will not
+                      imageheight = this.height; // work for in memory images.
+                      $('#userprofilepicture_upload').jrac({'crop_width': cropwidth, 'crop_height': cropheight, 'image_width': imagewidth, 'image_height': imageheight, 'viewport_width': imagewidth, 'viewport_height': imageheight, 'viewport_onload': function(){
+                                        console.log('Viewport loaded.');
+                                        $(".jrac_zoom_slider").hide();
+                                        $(".ui-resizable-handle").hide();
+                                           var $viewport = this;
+                                           $viewport.observator.register('jrac_crop_x', $(document), function(element, event_name, value){
+                                              cropx = value;
+                                              //console.log("x, y: ", cropx, cropy);
+                                              mycontext.drawImage($("#userprofilepicture_upload")[0], cropx, cropy, cropwidth, cropheight, 0, 0, coverphoto_width, coverphoto_height);
+                                              retinacontext.drawImage($("#userprofilepicture_upload")[0], cropx, cropy, cropwidth, cropheight, 0, 0, retina_width, retina_height);
+                                              //var tempimagedata = tempcanvas.toDataURL();
+                                              //$("#userprofilepicture_preview").attr("src", tempimagedata);
+                                              //console.log($(".jrac_crop_drag_handler").position());
+                                              var imagedata = mycanvas.toDataURL();
+                                              var retinaImageData = retinacanvas.toDataURL();
+                                              $("#userprofilepicture_preview").attr("src", imagedata);
+                                              $("#userprofilepicture_preview").attr("width", coverphoto_width/2);
+                                              $("#userprofilepicture_preview").attr("height", coverphoto_height/2);
+                                              $("#userprofilepicture_retina").attr("src", retinaImageData);
+                                            });
+                                           $viewport.observator.register('jrac_crop_y', $(document), function(element, event_name, value){
+                                              cropy = value;
+                                              //console.log("x, y: ", cropx, cropy);
+                                              mycontext.drawImage($("#userprofilepicture_upload")[0], cropx, cropy, cropwidth, cropheight, 0, 0, coverphoto_width, coverphoto_height);
+                                              //var tempimagedata = tempcanvas.toDataURL();
+                                              //$("#userprofilepicture_preview").attr("src", tempimagedata);
+                                              //console.log($(".jrac_crop_drag_handler").position());
+                                              var imagedata = mycanvas.toDataURL();
+                                              var retinaImageData = retinacanvas.toDataURL();
+                                              $("#userprofilepicture_preview").attr("src", imagedata);
+                                              $("#userprofilepicture_preview").attr("width", coverphoto_width/2);
+                                              $("#userprofilepicture_preview").attr("height", coverphoto_height/2);
+                                              $("#userprofilepicture_retina").attr("src", retinaImageData);
+                                            });
+                                      }
+                      });
+                  });
+              
+
+
+
+            });
+
           };
         })(f);
 
@@ -174,51 +255,67 @@ Template.userProfileEdit.events({
               // Apply jrac on some image.
               var tempcanvas = document.createElement('canvas');
               var tempcontext = tempcanvas.getContext('2d');
-              $('#userprofilepicture_upload').jrac({'viewport_onload': function(){
-                  console.log('Viewport loaded.');
-                     var $viewport = this;
-                     $viewport.observator.register('jrac_crop_x', $(document), function(element, event_name, value){console.log("x: ", value);});
-                     $viewport.observator.register('jrac_crop_y', $(document), function(element, event_name, value){console.log("y: ", value);});
-                }
-              });
+              var mycanvas = document.createElement('canvas');
+              var retinacanvas = document.createElement('canvas');
+              var mycontext = mycanvas.getContext('2d');
+              var retinacontext = retinacanvas.getContext('2d');
+              mycanvas.width = coverphoto_width;
+              mycanvas.height = coverphoto_height;
+              retinacanvas.width = retina_width;
+              retinacanvas.height = retina_height;
+              var cropx, cropy, cropwidth, cropheight, imagewidth, imageheight;
+              cropwidth = 150;
+              cropheight = 150;
+              var img = $("#userprofilepicture_upload")[0]; // Get my img elem
+              var pic_real_width, pic_real_height;
+              $("<img/>") // Make in memory copy of image to avoid css issues
+                  .attr("src", $(img).attr("src"))
+                  .load(function() {
+                      imagewidth = this.width;   // Note: $(this).width() will not
+                      imageheight = this.height; // work for in memory images.
+                      $('#userprofilepicture_upload').jrac({'crop_width': cropwidth, 'crop_height': cropheight, 'image_width': imagewidth, 'image_height': imageheight, 'viewport_width': imagewidth, 'viewport_height': imageheight, 'viewport_onload': function(){
+                                        console.log('Viewport loaded.');
+                                        $(".jrac_zoom_slider").hide();
+                                        $(".ui-resizable-handle").hide();
+                                           var $viewport = this;
+                                           $viewport.observator.register('jrac_crop_x', $(document), function(element, event_name, value){
+                                              cropx = value;
+                                              //console.log("x, y: ", cropx, cropy);
+                                              mycontext.drawImage($("#userprofilepicture_upload")[0], cropx, cropy, cropwidth, cropheight, 0, 0, coverphoto_width, coverphoto_height);
+                                              retinacontext.drawImage($("#userprofilepicture_upload")[0], cropx, cropy, cropwidth, cropheight, 0, 0, retina_width, retina_height);
+                                              //var tempimagedata = tempcanvas.toDataURL();
+                                              //$("#userprofilepicture_preview").attr("src", tempimagedata);
+                                              //console.log($(".jrac_crop_drag_handler").position());
+                                              var imagedata = mycanvas.toDataURL();
+                                              var retinaImageData = retinacanvas.toDataURL();
+                                              $("#userprofilepicture_preview").attr("src", imagedata);
+                                              $("#userprofilepicture_preview").attr("width", coverphoto_width/2);
+                                              $("#userprofilepicture_preview").attr("height", coverphoto_height/2);
+                                              $("#userprofilepicture_retina").attr("src", retinaImageData);
+                                            });
+                                           $viewport.observator.register('jrac_crop_y', $(document), function(element, event_name, value){
+                                              cropy = value;
+                                              //console.log("x, y: ", cropx, cropy);
+                                              mycontext.drawImage($("#userprofilepicture_upload")[0], cropx, cropy, cropwidth, cropheight, 0, 0, coverphoto_width, coverphoto_height);
+                                              //var tempimagedata = tempcanvas.toDataURL();
+                                              //$("#userprofilepicture_preview").attr("src", tempimagedata);
+                                              //console.log($(".jrac_crop_drag_handler").position());
+                                              var imagedata = mycanvas.toDataURL();
+                                              var retinaImageData = retinacanvas.toDataURL();
+                                              $("#userprofilepicture_preview").attr("src", imagedata);
+                                              $("#userprofilepicture_preview").attr("width", coverphoto_width/2);
+                                              $("#userprofilepicture_preview").attr("height", coverphoto_height/2);
+                                              $("#userprofilepicture_retina").attr("src", retinaImageData);
+                                            });
+                                      }
+                      });
+                  });
+              
 
 
 
             });
-            /*$(document).ready( function(){
-              $(function(){
-                function showPreview(coords){
-                  var mycanvas = document.createElement('canvas');
-                  var retinacanvas = document.createElement('canvas');
-                  var mycontext = mycanvas.getContext('2d');
-                  var retinacontext = retinacanvas.getContext('2d');
-                  mycanvas.width = coverphoto_width;
-                  mycanvas.height = coverphoto_height;
-                  retinacanvas.width = retina_width;
-                  retinacanvas.height = retina_height;
-                  console.log(coords);
-                  mycontext.drawImage($("#userprofilepicture_upload")[0], coords.x, coords.y, (coords.x2 - coords.x), (coords.y2 - coords.y), 0, 0, coverphoto_width, coverphoto_height);
-                  retinacontext.drawImage($("#userprofilepicture_upload")[0], coords.x, coords.y, (coords.x2 - coords.x), (coords.y2 - coords.y), 0, 0, retina_width, retina_height);
-                  var imagedata = mycanvas.toDataURL();
-                  var retinaImageData = retinacanvas.toDataURL();
-                  $("#userprofilepicture_preview").attr("src", imagedata);
-                  $("#userprofilepicture_preview").attr("width", coverphoto_width/2);
-                  $("#userprofilepicture_preview").attr("height", coverphoto_height/2);
-                  $("#userprofilepicture_retina").attr("src", retinaImageData);
-                };
 
-                $('#userprofilepicture_upload').Jcrop({
-                  onChange: showPreview,
-                  onSelect: showPreview,
-                  setSelect:   [ 50, 50, coverphoto_width, coverphoto_height ],
-                  aspectRatio: coverphoto_width/coverphoto_height
-                }, function(){
-                  jcrop_api = this;
-                  jcrop_api.setOptions({ allowResize: false });
-                });
-
-              });
-            });*/
           };
         })(f);
 
