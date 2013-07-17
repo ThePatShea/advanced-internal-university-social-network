4abd5848a9a2a165a50dbf6c41d82f1146b7595f
Pat Shea <development@thecampusbubble.com>
2013-07-16 23:38:58 -0400

    front end

--- sandbox/bubble_meteor/client/views/posts/post_page/post_page.js
+++ sandbox/bubble_meteor/client/views/posts/post_page/post_page.js
@@ -8,18 +8,29 @@ Template.postPage.helpers({
 });
 
 Template.postPage.events({
-  'click .btn-flag': function() {
-    //Google Analytics
-    _gaq.push(['_trackEvent', 'Flagging', 'Flag', +this.name]);
+    'click .btn-flag': function() {
+      //Google Analytics
+      _gaq.push(['_trackEvent', 'Flagging', 'Flag', +this.name]);
 
-    if (confirm("Flagging a post will report it to Bubble moderators as inappropriate. Are you sure you want to flag this post?")) {
-      var flagAttributes = {
-        postId: this._id,
-        bubbleId: this.bubbleId,
-        invokerId: Meteor.userId(),
-        invokerName: Meteor.user().username,
+      if (confirm("Flagging a post will report it to Bubble moderators as inappropriate. Are you sure you want to flag this post?")) {
+        var flagAttributes = {
+          postId: this._id,
+          bubbleId: this.bubbleId,
+          invokerId: Meteor.userId(),
+          invokerName: Meteor.user().username,
+        }
+        Meteor.call('createFlag',flagAttributes);
       }
-      Meteor.call('createFlag',flagAttributes);
     }
-  }
+  , 'click .delete': function(e) {
+      //Google Analytics
+        _gaq.push(['_trackEvent', 'Post', 'Delete Discussion', this.name]);
+
+      e.preventDefault();
+      if (confirm("Delete this post?")) {
+        var currentPostId = Session.get('currentPostId');
+        Posts.remove(currentPostId);
+        Meteor.Router.to('bubblePage',Session.get('currentBubbleId'));
+      }
+    }
 });
