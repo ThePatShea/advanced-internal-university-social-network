6c7b8ca52c634a8725f6e27ab21845f8090a302b
fernandes.praphat@gmail.com <fernandes.praphat@gmail.com>
2013-07-18 14:00:20 -0400

    Added the file size to the helpers and styling for the single fiel post page.

--- sandbox/bubble_meteor/client/views/posts/post_attributes/post_attributes.js
+++ sandbox/bubble_meteor/client/views/posts/post_attributes/post_attributes.js
@@ -4,5 +4,26 @@ Template.postAttributes.helpers({
         return true;
       else
         return false;
+    },
+
+    fileSizeWords: function(){
+    	if(typeof this.fileSize != 'undefined'){
+    		var fileSizeBytes = this.fileSize;
+    		var fileSizeKiloBytes = this.fileSize/1000;
+    		var fileSizeMegaBytes = this.fileSize/1000000;
+    		var fileSizeGigaBytes = this.fileSize/1000000000;
+    		if(fileSizeGigaBytes >= 1){
+    			return Math.round(fileSizeGigaBytes*100)/100 + ' GB'; 
+    		}
+    		else if(fileSizeMegaBytes >= 1){
+    			return Math.round(fileSizeMegaBytes*100)/100 + ' MB';
+    		}
+    		else{
+    			return Math.round(fileSizeKiloBytes*100)/100 + ' KB';
+    		}
+    	}
+    	else{
+    		return -1;
+    	}
     }
 });
