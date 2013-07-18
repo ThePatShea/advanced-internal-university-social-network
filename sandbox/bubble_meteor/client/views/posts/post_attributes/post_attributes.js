90e92f19317a3b9e13f6c9a10d0a2ab0e7a56c0c
Pat Shea <development@thecampusbubble.com>
2013-07-18 01:43:32 -0400

    front end

--- sandbox/bubble_meteor/client/views/posts/post_attributes/post_attributes.js
+++ sandbox/bubble_meteor/client/views/posts/post_attributes/post_attributes.js
@@ -1,6 +1,6 @@
 Template.postAttributes.helpers({
     hasChildren: function() {
-      if (this.children != undefined)
+      if (this.children != undefined && this.children.length > 0)
         return true;
       else
         return false;
