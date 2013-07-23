93c6bb7ece76c24ac0053cbaa060e6c5bdc63edf
Praphat Xavier Fernandes <fernandes.praphat@gmail.com>
2013-07-23 23:51:07 +0000

    Added a shell script that does take a backup of the Bubble Meteor site.

--- sandbox/bubble_backup/runbackup.py
+++ sandbox/bubble_backup/runbackup.py
@@ -2,14 +2,15 @@
 
 import datetime
 import os
+import subprocess
 
 
 current_backup_path = '~/bubblebackup_data/'
 mongodump_username = 'client'
-mongodump_password = '321524e6-680c-1fc3-5e99-27da50c1a3de'
-mongodump_host = 'production-db-a1.meteor.io'
+mongodump_password = '569e356f-148a-f982-e429-ed09521ee2b3'
+mongodump_host = 'production-db-a1.meteor.io:27017'
 mongodump_port = '27017'
-mongodump_collection = 'bubble_meteor_com'
+mongodump_database = 'bubble_meteor_com'
 
 current_datetime = datetime.datetime.now()
 current_backup_foldername = 'mongodump-' + current_datetime.strftime('%Y-%m-%d--%H_%M_%S')
@@ -18,6 +19,8 @@ current_backup_path = os.path.join(current_backup_path, current_backup_foldernam
 print current_backup_path
 
 #os.system(''mongodb://client:321524e6-680c-1fc3-5e99-27da50c1a3de@production-db-a1.meteor.io:27017/bubble_meteor_com')
-os.system('mongodump --username ' + mongodump_username + ' --password ' + mongodump_password + ' --host ' + mongodump_host + ' --port ' + mongodump_port + ' -d ' + mongodump_collection + ' --out ' + current_backup_path)
+#mongodump -u client -h production-db-a1.meteor.io:27017 -d bubble_meteor_com -p dc741741-bae1-0a4b-7c75-16d506469b5d
 
-
+mongodump_exec_string = 'mongodump -u ' + mongodump_username + ' -h ' + mongodump_host +  ' -d ' + mongodump_database + ' -p ' + mongodump_password +   ' --out ' + current_backup_path
+print mongodump_exec_string
+os.system(mongodump_exec_string)
