ae3a96a3093c49aaca5ca56c1849ccd66c4a44ff
Pat Shea <development@thecampusbubble.com>
2013-07-24 09:06:45 -0400

    front end

--- sandbox/bubble_meteor/client/views/posts/post_submit/file_submit/file_submit.js
+++ sandbox/bubble_meteor/client/views/posts/post_submit/file_submit/file_submit.js
@@ -52,51 +52,16 @@ Template.fileSubmit.events({
         reader.onload = (function(theFile) {
         return function(e) {
         // Render thumbnail.
-        var span = document.createElement('span');
-        span.innerHTML = ['<img class="previewthumb" src="', e.target.result,
+        var li = document.createElement('li');
+        li.innerHTML = ['<img class="previewthumb" src="', e.target.result,
                         '" title="', escape(theFile.name), '"/>'].join('');
-        document.getElementById('list').insertBefore(span, null);
+        document.getElementById('list').insertBefore(li, null);
         };
         })(f);
 
         // Read in the image file as a data URL.
         reader.readAsDataURL(f);
-      }
-
-      //If it is a PDF then render a PDF icon
-      else if (f.type.match('pdf.*')){
-        var reader = new FileReader();
-
-        // Closure to capture the file information.
-        reader.onload = (function(theFile) {
-        return function(e) {
-        //Render a PDF icon
-        var span = document.createElement('span');
-        span.innerHTML = ['<div class="pdf-icon">', 'PDF', escape(theFile.name), '</div>'].join('');
-        document.getElementById('list').insertBefore(span, null);
-        };
-        })(f);
-
-        //Read file as a data url.
-        reader.readAsDataURL(f);
-      }
-      else if (f.type.match('msword.*') || f.type.match('ms-excel.*') || f.type.match('officedocument.*')){
-        var reader = new FileReader();
-
-        // Closure to capture the file information.
-        reader.onload = (function(theFile) {
-        return function(e) {
-        //Render a PDF icon
-        var span = document.createElement('span');
-        span.innerHTML = ['<div class="word-icon">', 'Word Document: ', escape(theFile.name), '</div>'].join('');
-        document.getElementById('list').insertBefore(span, null);
-        };
-        })(f);
-
-        //Read file as a data url.
-        reader.readAsDataURL(f);
-      }
-      else{
+      } else {
         console.log('Else');
         var reader = new FileReader();
 
@@ -104,6 +69,9 @@ Template.fileSubmit.events({
         reader.onload = (function(theFile) {
           return function(e) {
             console.log(e.target.result);
+            var li = document.createElement('li');
+            li.innerHTML = ["<div class='add-padding'><div class='cb-icon cb-icon-file'> <svg version='1.1' id='Layer_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='32.041px' height='31.966px' viewBox='0 0 32.041 31.966' enable-background='new 0 0 32.041 31.966' xml:space='preserve'> <path fill-rule='evenodd' clip-rule='evenodd' d='M30,7V6H12V2H2v8h10h18V7z M2,17v13h2h6h20V12H7H4H2V17z M31,32H13H7H1 c-0.55,0-1-0.45-1-1V1c0-0.55,0.45-1,1-1h12c0.55,0,1,0.45,1,1v3h17c0.549,0,1,0.45,1,1v26C32,31.55,31.549,32,31,32z'/></svg></div><div class='cb-icon-lbl file-name'>" + theFile.name + "</div></div>"].join('');
+            document.getElementById('list').insertBefore(li, null);
           };
         })(f);
 
