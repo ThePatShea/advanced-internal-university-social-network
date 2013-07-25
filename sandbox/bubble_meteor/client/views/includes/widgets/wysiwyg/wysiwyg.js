c5b4d9262dd223717779558464c98e29f2678285
Pat Shea <development@thecampusbubble.com>
2013-07-25 15:11:39 -0400

    front end

--- sandbox/bubble_meteor/client/views/includes/widgets/wysiwyg/wysiwyg.js
+++ sandbox/bubble_meteor/client/views/includes/widgets/wysiwyg/wysiwyg.js
@@ -81,11 +81,8 @@ Template.wysiwyg.rendered = function() {
       if(attachment.fileType.match('image.*')){
         $("#list").append('<li><img class="previewthumb" src="' + attachment.file + '"/></li>');
       }
-      else if(attachment.fileType.match('pdf.*')){
-        $("#list").append('<div class="pdf-icon">'+ attachment.name+ '<a class="saved-remove btn btn-danger" id="'+ attachment._id+'">Remove</a></div>');
-      }
-      else if(attachment.fileType.match('msword.*')|| attachment.fileType.match('ms-excel.*') || attachment.fileType.match('officedocument.*')){
-        $("#list").append('<div class="word-icon">'+ attachment.name+ '<a class="saved-remove btn btn-danger" id="'+ attachment._id+'">Remove</a></div>');
+      else {
+        $("#list").append("<div class='add-padding'><div class='cb-icon cb-icon-file'> <svg version='1.1' id='Layer_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='32.041px' height='31.966px' viewBox='0 0 32.041 31.966' enable-background='new 0 0 32.041 31.966' xml:space='preserve'> <path fill-rule='evenodd' clip-rule='evenodd' d='M30,7V6H12V2H2v8h10h18V7z M2,17v13h2h6h20V12H7H4H2V17z M31,32H13H7H1 c-0.55,0-1-0.45-1-1V1c0-0.55,0.45-1,1-1h12c0.55,0,1,0.45,1,1v3h17c0.549,0,1,0.45,1,1v26C32,31.55,31.549,32,31,32z'/></svg></div><div class='cb-icon-lbl file-name'>" + theFile.name + "</div></div>");
       }
     }
   }
@@ -110,99 +107,6 @@ Template.wysiwyg.events({
   },
 
   'drop #drop_zone': function(evt){
-    /*l = document.getElementById('list');
-    while(l.hasChildNodes()){
-      l.removeChild(l.lastChild);
-    };
-    console.log('Drop');
-        evt.stopPropagation();
-    evt.preventDefault();
-
-    if(typeof files == 'undefined'){
-      files = [];
-     for(var i = 0, f; f=evt.dataTransfer.files[i]; i++){
-        files.push(evt.dataTransfer.files[i]);
-      };
-    }
-    else{
-      console.log(files);
-      for(var i = 0, f; f=evt.dataTransfer.files[i]; i++){
-        files.push(evt.dataTransfer.files[i]);
-      };
-    }
-    
-    for (var i = 0, f; f = files[i]; i++) {
-
-
-      //If it is an image then render a thumbnail
-      if (f.type.match('image.*')) {
-        var reader = new FileReader();
-
-        // Closure to capture the file information.
-        reader.onload = (function(theFile) {
-        return function(e) {
-        // Render thumbnail.
-        var span = document.createElement('span');
-        span.innerHTML = ['<img class="previewthumb" src="', e.target.result,
-                        '" title="', escape(theFile.name), '"/>'].join('');
-        document.getElementById('list').insertBefore(span, null);
-        };
-        })(f);
-
-        // Read in the image file as a data URL.
-        reader.readAsDataURL(f);
-      }
-
-      //If it is a PDF then render a PDF icon
-      else if (f.type.match('pdf.*')){
-        var reader = new FileReader();
-
-        // Closure to capture the file information.
-        reader.onload = (function(theFile) {
-        return function(e) {
-        //Render a PDF icon
-        var span = document.createElement('span');
-        span.innerHTML = ['<div class="pdf-icon">', 'PDF', escape(theFile.name), '</div>'].join('');
-        document.getElementById('list').insertBefore(span, null);
-        };
-        })(f);
-
-        //Read file as a data url.
-        reader.readAsDataURL(f);
-      }
-      else if (f.type.match('msword.*') || f.type.match('ms-excel.*') || f.type.match('officedocument.*')){
-        var reader = new FileReader();
-
-        // Closure to capture the file information.
-        reader.onload = (function(theFile) {
-        return function(e) {
-        //Render a PDF icon
-        var span = document.createElement('span');
-        span.innerHTML = ['<div class="word-icon">', 'Word Document: ', escape(theFile.name), '</div>'].join('');
-        document.getElementById('list').insertBefore(span, null);
-        };
-        })(f);
-
-        //Read file as a data url.
-        reader.readAsDataURL(f);
-      }
-      else{
-        console.log('Else');
-        var reader = new FileReader();
-
-        // Closure to capture the file information.
-        reader.onload = (function(theFile) {
-          return function(e) {
-            console.log(e.target.result);
-          };
-        })(f);
-
-        reader.readAsDataURL(f);
-      }
-
-
-    }*/
-
     console.log(evt.dataTransfer.files);
 
     evt.stopPropagation();
@@ -244,11 +148,8 @@ Template.wysiwyg.events({
           if(attachment.fileType.match('image.*')){
             $("#list").append('<li><img class="previewthumb" src="' + attachment.file + '"/></li>');
           }
-          else if(attachment.fileType.match('pdf.*')){
-            $("#list").append('<div class="pdf-icon">'+ attachment.name+ '<a class="saved-remove btn btn-danger" id="'+ attachment._id+'">Remove</a></div>');
-          }
-          else if(attachment.fileType.match('msword.*')|| attachment.fileType.match('ms-excel.*') || attachment.fileType.match('officedocument.*')){
-            $("#list").append('<div class="word-icon">'+ attachment.name+ '<a class="saved-remove btn btn-danger" id="'+ attachment._id+'">Remove</a></div>');
+          else {
+            $("#list").append("<div class='add-padding'><div class='cb-icon cb-icon-file'> <svg version='1.1' id='Layer_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='32.041px' height='31.966px' viewBox='0 0 32.041 31.966' enable-background='new 0 0 32.041 31.966' xml:space='preserve'> <path fill-rule='evenodd' clip-rule='evenodd' d='M30,7V6H12V2H2v8h10h18V7z M2,17v13h2h6h20V12H7H4H2V17z M31,32H13H7H1 c-0.55,0-1-0.45-1-1V1c0-0.55,0.45-1,1-1h12c0.55,0,1,0.45,1,1v3h17c0.549,0,1,0.45,1,1v26C32,31.55,31.549,32,31,32z'/></svg></div><div class='cb-icon-lbl file-name'>" + theFile.name + "</div></div>");
           }
         }
       }
@@ -280,9 +181,7 @@ Template.wysiwyg.events({
         // Read in the image file as a data URL.
         reader.readAsDataURL(f);
       }
-
-      //If it is a PDF then render a PDF icon
-      else if (f.type.match('pdf.*')){
+      else if (f.type.match('pdf.*') || f.type.match('msword.*') || f.type.match('ms-excel.*') || f.type.match('officedocument.*')){
         var reader = new FileReader();
 
         // Closure to capture the file information.
@@ -290,26 +189,10 @@ Template.wysiwyg.events({
         return function(e) {
         //Render a PDF icon
         var fname = theFile.name;
-        var span = document.createElement('span');
-        span.innerHTML = ['<div class="pdf-icon">', 'PDF', fname, ' <a class="unsaved-remove btn btn-danger" id="', fname,'">Remove</a>', '</div>'].join('');
-        document.getElementById('list').insertBefore(span, null);
-        };
-        })(f);
 
-        //Read file as a data url.
-        reader.readAsDataURL(f);
-      }
-      else if (f.type.match('msword.*') || f.type.match('ms-excel.*') || f.type.match('officedocument.*')){
-        var reader = new FileReader();
-
-        // Closure to capture the file information.
-        reader.onload = (function(theFile) {
-        return function(e) {
-        //Render a PDF icon
-        var fname = theFile.name;
-        var span = document.createElement('span');
-        span.innerHTML = ['<div class="word-icon">', 'Word Document: ', fname, ' <a class="unsaved-remove btn btn-danger" id="', fname,'">Remove</a>', '</div>'].join('');
-        document.getElementById('list').insertBefore(span, null);
+        var li = document.createElement('li');
+        li.innerHTML = ["<div class='add-padding'><div class='cb-icon cb-icon-file'> <svg version='1.1' id='Layer_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='32.041px' height='31.966px' viewBox='0 0 32.041 31.966' enable-background='new 0 0 32.041 31.966' xml:space='preserve'> <path fill-rule='evenodd' clip-rule='evenodd' d='M30,7V6H12V2H2v8h10h18V7z M2,17v13h2h6h20V12H7H4H2V17z M31,32H13H7H1 c-0.55,0-1-0.45-1-1V1c0-0.55,0.45-1,1-1h12c0.55,0,1,0.45,1,1v3h17c0.549,0,1,0.45,1,1v26C32,31.55,31.549,32,31,32z'/></svg></div><div class='cb-icon-lbl file-name'>" + theFile.name + "</div></div>"].join('');
+        document.getElementById('list').insertBefore(li, null);
         };
         })(f);
 
@@ -372,11 +255,8 @@ Template.wysiwyg.events({
           if(attachment.fileType.match('image.*')){
             $("#list").append('<li><img class="previewthumb" src="' + attachment.file + '"/></li>');
           }
-          else if(attachment.fileType.match('pdf.*')){
-            $("#list").append('<div class="pdf-icon">'+ attachment.name+ '<a class="saved-remove btn btn-danger" id="'+ attachment._id+'">Remove</a></div>');
-          }
-          else if(attachment.fileType.match('msword.*')|| attachment.fileType.match('ms-excel.*') || attachment.fileType.match('officedocument.*')){
-            $("#list").append('<div class="word-icon">'+ attachment.name+ '<a class="saved-remove btn btn-danger" id="'+ attachment._id+'">Remove</a></div>');
+          else if(attachment.fileType.match('pdf.*') || attachment.fileType.match('msword.*')|| attachment.fileType.match('ms-excel.*') || attachment.fileType.match('officedocument.*')){
+            $("#list").append("<div class='add-padding'><div class='cb-icon cb-icon-file'> <svg version='1.1' id='Layer_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='32.041px' height='31.966px' viewBox='0 0 32.041 31.966' enable-background='new 0 0 32.041 31.966' xml:space='preserve'> <path fill-rule='evenodd' clip-rule='evenodd' d='M30,7V6H12V2H2v8h10h18V7z M2,17v13h2h6h20V12H7H4H2V17z M31,32H13H7H1 c-0.55,0-1-0.45-1-1V1c0-0.55,0.45-1,1-1h12c0.55,0,1,0.45,1,1v3h17c0.549,0,1,0.45,1,1v26C32,31.55,31.549,32,31,32z'/></svg></div><div class='cb-icon-lbl file-name'>" + theFile.name + "</div></div>");
           }
         }
       }
@@ -409,7 +289,7 @@ Template.wysiwyg.events({
       }
 
       //If it is a PDF then render a PDF icon
-      else if (f.type.match('pdf.*')){
+      else if (f.type.match('pdf.*') || f.type.match('msword.*') || f.type.match('ms-excel.*') || f.type.match('officedocument.*')){
         var reader = new FileReader();
 
         // Closure to capture the file information.
@@ -417,26 +297,10 @@ Template.wysiwyg.events({
         return function(e) {
         //Render a PDF icon
         var fname = theFile.name;
-        var span = document.createElement('span');
-        span.innerHTML = ['<div class="pdf-icon">', 'PDF', fname, ' <a class="unsaved-remove btn btn-danger" id="', fname,'">Remove</a>', '</div>'].join('');
-        document.getElementById('list').insertBefore(span, null);
-        };
-        })(f);
 
-        //Read file as a data url.
-        reader.readAsDataURL(f);
-      }
-      else if (f.type.match('msword.*') || f.type.match('ms-excel.*') || f.type.match('officedocument.*')){
-        var reader = new FileReader();
-
-        // Closure to capture the file information.
-        reader.onload = (function(theFile) {
-        return function(e) {
-        //Render a PDF icon
-        var fname = theFile.name;
-        var span = document.createElement('span');
-        span.innerHTML = ['<div class="word-icon">', 'Word Document: ', fname, ' <a class="unsaved-remove btn btn-danger" id="', fname,'">Remove</a>', '</div>'].join('');
-        document.getElementById('list').insertBefore(span, null);
+        var li = document.createElement('li');
+        li.innerHTML = ["<div class='add-padding'><div class='cb-icon cb-icon-file'> <svg version='1.1' id='Layer_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='32.041px' height='31.966px' viewBox='0 0 32.041 31.966' enable-background='new 0 0 32.041 31.966' xml:space='preserve'> <path fill-rule='evenodd' clip-rule='evenodd' d='M30,7V6H12V2H2v8h10h18V7z M2,17v13h2h6h20V12H7H4H2V17z M31,32H13H7H1 c-0.55,0-1-0.45-1-1V1c0-0.55,0.45-1,1-1h12c0.55,0,1,0.45,1,1v3h17c0.549,0,1,0.45,1,1v26C32,31.55,31.549,32,31,32z'/></svg></div><div class='cb-icon-lbl file-name'>" + theFile.name + "</div></div>"].join('');
+        document.getElementById('list').insertBefore(li, null);
         };
         })(f);
 
