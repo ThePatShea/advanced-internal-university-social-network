1f36b9abb7ac87ee48c56c4011d7ff3e1728bf8d
fernandes.praphat@gmail.com <fernandes.praphat@gmail.com>
2013-07-25 16:22:22 -0400

    Users get a default profile picture when they sign up.

--- sandbox/bubble_meteor/client/views/login/login_page.js
+++ sandbox/bubble_meteor/client/views/login/login_page.js
@@ -31,6 +31,22 @@ Template.loginPage.events({
       var username = $(event.target).find('[name=username]').val();
       var password = $(event.target).find('[name=password]').val();
       var email = $(event.target).find('[name=email]').val();
+      var tempcanvas = document.createElement('canvas');
+      var retinacanvas = document.createElement('canvas');
+      var tempcontext = tempcanvas.getContext('2d');
+      var retinacontext = retinacanvas.getContext('2d');
+      //$(document).append('<img src="/img/letterprofiles/a.jpg" id="tempprofile">');
+      $('#tempprofile').hide();
+      tempcontext.drawImage($('#tempprofile')[0], 0, 0, 160, 160);
+      retinacontext.drawImage($('#tempprofile')[0], 0, 0, 320, 320);
+      var profilePictureData = tempcanvas.toDataURL();
+      var retinaProfilePictureData = retinacanvas.toDataURL();
+      //$(document).append('<img src="" id="profilePicture">');
+      //$(document).append('<img src="" id="retinaProfilePicture">');
+      $('#profilePicture').hide();
+      $('#retinaProfilePicture').hide()
+      $('#profilePicture').attr('src', profilePictureData);
+      $('#retinaProfilePicture').attr('src', retinaProfilePictureData);
       if(username && password && email ){
         Accounts.createUser({
           username: username,
@@ -41,6 +57,16 @@ Template.loginPage.events({
           if(err){
             //Do something with error
           }else{
+            var profileProperties = {
+              profilePicture: profilePictureData,
+              retinaProfilePicture: retinaProfilePictureData
+            };
+            //console.log(profilePictureData);
+            //console.log($('#profilePicture').attr('src'));
+            var userid = Meteor.userId();
+            Meteor.users.update(userid, {$set: profileProperties}, function(err){
+              console.log(err);
+            });
             Meteor.Router.to('searchBubbles');
           }
         });
@@ -87,6 +113,10 @@ Template.loginPage.rendered = function(){
   // }else{
   //   Session.set('signup', 'none');
   // }
+  $('#tempprofile').hide();
+  $('#profilePicture').hide();
+  $('#retinaProfilePicture').hide();
+
   if(Session.get('secret') && Session.get('secret') != null){
     var secret = Session.get('secret');
     setTimeout(function(){
