86ef1c69d15959d3f9d2b91b88ef1d69254025b1
Pat Shea <development@thecampusbubble.com>
2013-07-26 06:09:16 -0400

    Updating the dropzone for uploading attachments to discussions. Starting to work.

--- sandbox/bubble_meteor/client/views/includes/widgets/wysiwyg/wysiwyg.js
+++ sandbox/bubble_meteor/client/views/includes/widgets/wysiwyg/wysiwyg.js
@@ -186,68 +186,26 @@ Template.wysiwyg.events({
     }
 
     for (var i = 0, f; f = files[i]; i++) {
-      //If it is an image then render a thumbnail
-      if (f.type.match('image.*')) {
-        var reader = new FileReader();
+      var reader = new FileReader();
 
-        reader.tmpl           =  tmpl;
-        reader.tmpl.fileType  =  f.type;
+      reader.tmpl           =  tmpl;
+      reader.tmpl.fileType  =  f.type;
 
-        // Closure to capture the file information.
-        reader.onload = (function(theFile) {
-        return function(e) {
-        var fname         =  theFile.name;
+      // Closure to capture the file information.
+      reader.onload = (function(theFile) {
+      return function(e) {
+      var fname         =  theFile.name;
 
-        // Render thumbnail.
-        theFile.file              =  e.target.result;
-        theFile.fileType          =  reader.tmpl.fileType;
-        this.getFileTypeControls  =  this.tmpl.getFileTypeControls;
-        this.fileTypeControls     =  this.tmpl.fileTypeControls;
+      // Render thumbnail.
+      theFile.fileType          =  reader.tmpl.fileType;
+      theFile.file              =  e.target.result;
 
-        this.tmpl.addToDropZone(theFile);
-/*
-        var li = document.createElement('li');
-        li.innerHTML = ['<img class="previewthumb" src="', e.target.result,
-                        '" title="', escape(theFile.name), '"/>'].join('');
-        document.getElementById('list').insertBefore(li, null);
-*/
-        };
-        })(f);
+      this.tmpl.addToDropZone(theFile);
+      };
+      })(f);
 
-        // Read in the image file as a data URL.
-        reader.readAsDataURL(f);
-      }
-      else if (f.type.match('pdf.*') || f.type.match('msword.*') || f.type.match('ms-excel.*') || f.type.match('officedocument.*')){
-        var reader = new FileReader();
-
-        // Closure to capture the file information.
-        reader.onload = (function(theFile) {
-        return function(e) {
-        //Render a PDF icon
-        var fname = theFile.name;
-
-        var li = document.createElement('li');
-        li.innerHTML = ["<div class='add-padding'><div class='cb-icon cb-icon-file'> <svg version='1.1' id='Layer_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='32.041px' height='31.966px' viewBox='0 0 32.041 31.966' enable-background='new 0 0 32.041 31.966' xml:space='preserve'> <path fill-rule='evenodd' clip-rule='evenodd' d='M30,7V6H12V2H2v8h10h18V7z M2,17v13h2h6h20V12H7H4H2V17z M31,32H13H7H1 c-0.55,0-1-0.45-1-1V1c0-0.55,0.45-1,1-1h12c0.55,0,1,0.45,1,1v3h17c0.549,0,1,0.45,1,1v26C32,31.55,31.549,32,31,32z'/></svg></div><div class='cb-icon-lbl file-name'>" + theFile.name + "</div></div>"].join('');
-        document.getElementById('list').insertBefore(li, null);
-        };
-        })(f);
-
-        //Read file as a data url.
-        reader.readAsDataURL(f);
-      }
-      else{
-        console.log('Else');
-        var reader = new FileReader();
-
-        // Closure to capture the file information.
-        reader.onload = (function(theFile) {
-          return function(e) {
-            console.log(e.target.result);
-          };
-        })(f);
-
-        reader.readAsDataURL(f);
-      }
+      // Read in the image file as a data URL.
+      reader.readAsDataURL(f);
     }
 
 
