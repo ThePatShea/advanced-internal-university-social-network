4706f17c7d6c9ad1a07a1104e0edb1f004a5a25b
Pat Shea <development@thecampusbubble.com>
2013-07-28 06:31:59 -0400

    front end for discussion submit form

--- sandbox/bubble_meteor/client/views/posts/post_submit/discussion_submit/discussion_submit.js
+++ sandbox/bubble_meteor/client/views/posts/post_submit/discussion_submit/discussion_submit.js
@@ -1,22 +1,32 @@
+Template.discussionSubmit.created = function () {
+  this.validateForm = function() {
+    var count = 0;
+
+    $('.required').each(function(i) {
+      if( !$(this).hasClass('wysiwyg') && $(this).val() === '' && $(this).attr("name") != undefined ) {
+        count++;
+      } else if ( $(this).hasClass('wysiwyg') && $(this).html().trim().replace("<br>","").replace('<span class="wysiwyg-placeholder">Type here...</span>','') == "" ) {
+        count++;
+      }
+
+      if (count == 0) {
+        $('.cb-submit').prop('disabled', false);
+        $('.cb-submit').removeClass('ready-false');
+      } else {
+        $('.cb-submit').prop('disabled', true);
+        $('.cb-submit').addClass('ready-false');
+      }
+    });
+  }
+}
+
+Template.discussionSubmit.rendered = function () {
+  this.validateForm();
+}
+
 Template.discussionSubmit.events({
-    'load .required, keyup .required, propertychange .required, input .required, paste .required': function() {
-       var count = 0;
-
-       $('.required').each(function(i) {
-         if( !$(this).hasClass('wysiwyg') && $(this).val() === '' && $(this).attr("name") != undefined ) {
-           count++;
-         } else if ( $(this).hasClass('wysiwyg') && $(this).html().trim().replace("<br>","").replace('<span class="wysiwyg-placeholder">Type here...</span>','') == "" ) {
-           count++;
-         }
-
-         if (count == 0) {
-           $('.cb-submit').prop('disabled', false);
-           $('.cb-submit').removeClass('ready-false');
-         } else {
-           $('.cb-submit').prop('disabled', true);
-           $('.cb-submit').addClass('ready-false');
-         }
-       });
+    'keyup .required, propertychange .required, input .required, paste .required': function(evt, tmpl) {
+      tmpl.validateForm();
     }
   , 'submit form': function(event) {
       event.preventDefault();
