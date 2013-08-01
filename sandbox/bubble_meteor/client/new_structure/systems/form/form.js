456b510db702d77fa81a9d43e3d2d423266142a8
Pat Shea <development@thecampusbubble.com>
2013-07-31 23:01:29 -0400

    front end

--- sandbox/bubble_meteor/client/new_structure/systems/form/form.js
+++ sandbox/bubble_meteor/client/new_structure/systems/form/form.js
@@ -5,16 +5,40 @@ Handlebars.registerHelper("systemForm", {
   , formParams   : {
         bubble : {
             create : {
-                validate       : ["title", "category", "body"]
-              , templateName   : "formElementsBubbleCreate"
-              , isCollapsed    : "collapse-false"
-              , objectNameDash : "bubble-create"
-              , wysiwygHeading : "Description"
-              , arrowVisible   : "false"
+                validate           : ["title", "category", "body"]
+              , templateName       : "formElementsBubbleCreate"
+              , isCollapsed        : "collapse-false"
+              , objectNameDash     : "bubble-create"
+              , wysiwygHeading     : "Description"
+              , selectedBubbleType : "normal"
+              , arrowVisible       : "false"
             }
         }
     }
   , formElements : function(templateName) {
       return Template[templateName]();
+    }
+});
+
+
+Template.formElementsBubbleCreate.events({
+  'click .select-bubble-type > .normal': function(evt, tmpl) {
+    evt.preventDefault();
+    systemForm.formParams.bubble.create.selectedBubbleType = "normal";
+  }
+
+ , 'click .select-bubble-type > .super': function(evt, tmpl) {
+    evt.preventDefault();
+    systemForm.formParams.bubble.create.selectedBubbleType = "super";
+  }
+});
+
+
+Template.formElementsBubbleCreate.helpers({
+    isSelectedBubbleType: function(inputBubbleType) {
+      if (inputBubbleType == this.selectedBubbleType)
+        return true;
+      else
+        return false;
     }
 });
