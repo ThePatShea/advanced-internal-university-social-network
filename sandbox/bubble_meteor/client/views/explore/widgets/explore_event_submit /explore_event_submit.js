7caef69fad7efbe42e4cad9708e635619aa34d3a
fernandes.praphat@gmail.com <fernandes.praphat@gmail.com>
2013-08-04 15:31:35 -0400

    The Add Post and Cancel Post buttons on the Explore section cover pages are now styled properly and toggle correctly.

---
+++ sandbox/bubble_meteor/client/views/explore/widgets/explore_event_submit /explore_event_submit.js
@@ -0,0 +1,45 @@
+Template.exploreEventSubmit.created = function () {
+  this.validateForm = function() {
+    var count = 0;
+
+    $('.required').each(function(i) {
+      if( !$(this).hasClass('wysiwyg') && $(this).val() === '' && $(this).attr("name") != undefined ) {
+        count++;
+      } else if ( $(this).hasClass('wysiwyg') && $(this).html().trim().replace("<br>","").replace('<span class="wysiwyg-placeholder">Type here...</span>','') == "" ) {
+        count++;
+      }
+
+      if (count == 0) {
+        $('#cb-form-container-discussion .cb-submit').prop('disabled', false);
+        $('#cb-form-container-discussion .cb-submit').removeClass('ready-false');
+      } else {
+        $('#cb-form-container-discussion .cb-submit').prop('disabled', true);
+        $('#cb-form-container-discussion .cb-submit').addClass('ready-false');
+      }
+    });
+  }
+}
+
+Template.discussionSubmit.rendered = function () {
+  this.validateForm();
+}
+
+Template.discussionSubmit.events({
+    'keyup .required, propertychange .required, input .required, paste .required': function(evt, tmpl) {
+      tmpl.validateForm();
+    }
+  , 'submit form': function(event) {
+      event.preventDefault();
+      //Google Analytics
+      _gaq.push(['_trackEvent', 'Post', 'Create Discussion', $(event.target).find('[name=name]').val()]);
+      
+      createPostWithAttachments({
+        name: $(event.target).find('[name=name]').val(),
+        body: $(event.target).find('.wysiwyg').html(),
+        postType: 'discussion',
+        bubbleId: Session.get('currentBubbleId'),
+        children: []
+      }, files);
+      
+    }
+});
