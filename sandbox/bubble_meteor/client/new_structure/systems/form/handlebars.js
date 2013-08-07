aa5cf201f8de731a01cd47f72529c8455b53fed7
fernandes.praphat@gmail.com <fernandes.praphat@gmail.com>
2013-08-07 16:51:51 -0400

    Added a few functions to the system forms.

--- sandbox/bubble_meteor/client/new_structure/systems/form/handlebars.js
+++ sandbox/bubble_meteor/client/new_structure/systems/form/handlebars.js
@@ -36,9 +36,22 @@ Handlebars.registerHelper("systemForm", {
           
           for (var i = 0, f; f = files[i]; i++) {
             var reader = new FileReader();
+            //console.log('Testing f: ', f.type, f.size);
             reader.onload = (function(f){
               return function(e) {
                 console.log(f.type, f.size);
+                /*if(f.type.match('image.*')){
+                    createPost({
+                      name: escape(f.name),
+                      file: imageData,
+                      fileType: f.type,
+                      fileSize: f.size,
+                      postType: 'file',
+                      numDownloads: 0,
+                      lastDownloadTime: new Date().getTime(),
+                      bubbleId: Session.get('currentBubbleId')
+                    });
+                }*/
                 
                 createPost({
                   name: escape(f.name),
@@ -59,3 +72,43 @@ Handlebars.registerHelper("systemForm", {
     }
   }
 });
+
+
+
+
+//This function obtains the correct width and height of an image and then passes them onto a callback together with the imagedata
+var imageDimensions = function(imageDataURL, callback){
+  $('<img/>').attr('src', imageDataURL).load(function(){
+    callback(this.width, this.height, imageDataURL);
+  });
+
+}
+
+
+var storeImage = function(f){
+  var reader = new FileReader();
+  //console.log('Testing f: ', f.type, f.size);
+  reader.onload = (function(f){
+    return function(e){
+      $('<img />').attr('src', imageDataURL).load(function(){
+        var width = this.width;
+        var height = this.height;
+        var retinaCanvas = ($('<canvas/>').width(width*2).height(height*2))[0];
+        var retinaContext = retinaCanvas.getContext('2d');
+        var tempImage = new Image();
+        tempImage.src = this.src;
+        retinaContext.drawImage(coverImage, 0, 0, width, height, 0, 0, width*2, height*2);
+        createPost({
+          name: escape(f.name),
+          file: e.target.result,
+          fileType: f.type,
+          fileSize: f.size,
+          postType: 'file',
+          numDownloads: 0,
+          lastDownloadTime: new Date().getTime(),
+          bubbleId: Session.get('currentBubbleId')
+        });
+      });
+    }
+  });
+}
