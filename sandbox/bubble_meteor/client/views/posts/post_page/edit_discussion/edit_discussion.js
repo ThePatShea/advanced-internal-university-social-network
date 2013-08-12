443b9574ecda2eea4ff3f9d736a8753fb3bf1937
fernandes.praphat@gmail.com <fernandes.praphat@gmail.com>
2013-08-12 16:16:04 -0400

    Creating and editing discussions now works completely.

--- sandbox/bubble_meteor/client/views/posts/post_page/edit_discussion/edit_discussion.js
+++ sandbox/bubble_meteor/client/views/posts/post_page/edit_discussion/edit_discussion.js
@@ -84,20 +84,23 @@ function processAttachmentSelections(fileAttachments){
       discussionNewAttachments.push(fileAttachments[i]);
     }
     console.log(discussionNewAttachments);
-    /*if(discussionNewAttachments.length > 0){
+    if(discussionNewAttachments.length > 0){
+      $('.cb-editDiscussion-form > .paperclip-attach-files').hide();
       $('.cb-editDiscussion-form > .paperclip-attach-files > .paperclip-attach > .cb-paperclip-lbl').hide();
       $('.cb-editDiscussion-form > .paperclip-attach-files > .paperclip-attach > .cb-icon-attachment').hide();
       $('.cb-editDiscussion-form > .paperclip-attach-files > .paperclip-attach > .file-chooser-invisible').width(1);
       $('.cb-editDiscussion-form > .paperclip-attach-files > .paperclip-attach > .file-chooser-invisible').height(1);
-    }*/
+    }
     $('.cb-editDiscussion-form > .discussion-attachments > .paperclip-attach > .attachments-list').html('');
     for(var i=0; i < discussionAttachments.length; i++){
-      console.log('Attachment: ', discussionAttachments[i].name);
-      $('.cb-editDiscussion-form > .discussion-attachments > .paperclip-attach > .attachments-list').append('<li><span class="attachment-cancel-icon" id="' + discussionAttachments[i]._id + '"><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="16px" height="16px" viewBox="0 0 16 16" enable-background="new 0 0 16 16" xml:space="preserve"><circle fill-rule="evenodd" clip-rule="evenodd" cx="8" cy="8" r="8"/><g><rect x="7.001" y="4" transform="matrix(-0.7151 -0.699 0.699 -0.7151 8.1297 19.3133)" fill-rule="evenodd" clip-rule="evenodd" fill="#FFFFFF" width="2" height="8"/><rect x="7" y="4" transform="matrix(-0.6989 0.7152 -0.7152 -0.6989 19.3134 7.869)" fill-rule="evenodd" clip-rule="evenodd" fill="#FFFFFF" width="2" height="8"/></g></span><span class="attachment-list-filename">' + discussionAttachments[i].name +'</span></li>');
-      $('.cb-editDiscussion-form > .discussion-attachments > .paperclip-attach > .attachments-list > li > #'+discussionAttachments[i]._id).click(function(){
-        discussionDeletedAttachmentIds.push(this.id);
-        $(this).parent().remove();
-      });
+      if(discussionDeletedAttachmentIds.indexOf(discussionAttachments[i]._id) == -1){
+        console.log('Attachment: ', discussionAttachments[i].name);
+        $('.cb-editDiscussion-form > .discussion-attachments > .paperclip-attach > .attachments-list').append('<li><span class="attachment-cancel-icon" id="' + discussionAttachments[i]._id + '"><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="16px" height="16px" viewBox="0 0 16 16" enable-background="new 0 0 16 16" xml:space="preserve"><circle fill-rule="evenodd" clip-rule="evenodd" cx="8" cy="8" r="8"/><g><rect x="7.001" y="4" transform="matrix(-0.7151 -0.699 0.699 -0.7151 8.1297 19.3133)" fill-rule="evenodd" clip-rule="evenodd" fill="#FFFFFF" width="2" height="8"/><rect x="7" y="4" transform="matrix(-0.6989 0.7152 -0.7152 -0.6989 19.3134 7.869)" fill-rule="evenodd" clip-rule="evenodd" fill="#FFFFFF" width="2" height="8"/></g></span><span class="attachment-list-filename">' + discussionAttachments[i].name +'</span></li>');
+        $('.cb-editDiscussion-form > .discussion-attachments > .paperclip-attach > .attachments-list > li > #'+discussionAttachments[i]._id).click(function(){
+          discussionDeletedAttachmentIds.push(this.id);
+          $(this).parent().remove();
+        });
+      }
     }
     
     for (var i = 0, f; f = discussionNewAttachments[i]; i++) {
