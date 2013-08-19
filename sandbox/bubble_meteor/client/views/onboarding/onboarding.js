e3df7267152bbdfac5f4672d4cc9ab9f8c449d88
Pat Shea <development@thecampusbubble.com>
2013-08-19 04:02:36 -0400

    finishing the onboarding page

--- sandbox/bubble_meteor/client/views/onboarding/onboarding.js
+++ sandbox/bubble_meteor/client/views/onboarding/onboarding.js
@@ -11,10 +11,48 @@ Template.onboarding.events({
     $('#cb-form-container-onboarding .cb-submit').prop('disabled', false);
     $('#accept-terms').addClass('selected');
   },
+  'submit form': function(e) {
+    e.preventDefault();
+
+    var currentProfileId = Meteor.userId();
+
+    var profileProperties = {
+      profilePicture: $(e.target).find('[id=userprofilepicture_preview]').attr('src'),
+      retinaProfilePicture: $(e.target).find('[id=userprofilepicture_retina]').attr('src'),
+      lastUpdated: new Date().getTime(),
+    };
+
+    Meteor.users.update(currentProfileId, {$set: profileProperties}, function(error) {
+      if (error) {
+        throwError(error.reason);
+      } else {
+        Meteor.Router.to('dashboard');
+      }
+    });
+
+  },
 });
+
+
+
+
+
+
+
+
+
+
+
 
 
 Template.onboarding.rendered = function() {
   $('#cb-form-container-onboarding .cb-submit').addClass('ready-false');
   $('#cb-form-container-onboarding .cb-submit').prop('disabled', true);
 }
+
+
+
+
+
+
+
