9303908a7bcda16836a61295fce9eea56372a0c8
Pat Shea <development@thecampusbubble.com>
2013-08-18 22:49:19 -0400

    working on onboarding

--- sandbox/bubble_meteor/client/views/onboarding/onboarding.js
+++ sandbox/bubble_meteor/client/views/onboarding/onboarding.js
@@ -1,5 +1,20 @@
 Template.onboarding.helpers({
   getCurrentName: function() {
     return Meteor.user().username;
-  }
+  },
 });
+
+
+Template.onboarding.events({
+  'click #accept-terms': function() {
+    $('#cb-form-container-onboarding .cb-submit').removeClass('ready-false');
+    $('#cb-form-container-onboarding .cb-submit').prop('disabled', false);
+    $('#accept-terms').addClass('selected');
+  },
+});
+
+
+Template.onboarding.rendered = function() {
+  $('#cb-form-container-onboarding .cb-submit').addClass('ready-false');
+  $('#cb-form-container-onboarding .cb-submit').prop('disabled', true);
+}
