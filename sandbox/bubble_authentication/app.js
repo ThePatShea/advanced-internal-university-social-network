c8ffba69dd0a3bd6b7ff0fe787d99c7d78337ca6
Ubuntu <ubuntu@ip-10-99-65-13.ec2.internal>
2013-08-19 21:44:30 +0000

    Set the Bubble Augh node server to listen on port 3000

--- sandbox/bubble_authentication/app.js
+++ sandbox/bubble_authentication/app.js
@@ -120,7 +120,7 @@ var app = express();
 
 
 // all environments
-app.set('port', process.env.PORT || 80);
+app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(passport.initialize());
