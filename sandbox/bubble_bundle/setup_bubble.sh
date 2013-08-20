2ec6ee240da670d4cc4f41e2ea14f80716cad544
Ubuntu <ubuntu@ip-10-99-65-13.ec2.internal>
2013-08-20 21:00:12 +0000

    Updated the Bubble Authentication configs, setup and startup scripts.

--- sandbox/bubble_bundle/setup_bubble.sh
+++ sandbox/bubble_bundle/setup_bubble.sh
@@ -6,6 +6,7 @@ cd bundle/server/
 npm install fibers@1.0.0
 cd ../../
 cp ./nginx_config/bubble /etc/nginx/sites-available/bubble
+cp ./nginx_config/nginx.conf /etc/nginx/nginx.conf
 ln -s /etc/nginx/sites-available/bubble /etc/nginx/sites-enabled/bubble
+service nginx restart
 cp ./upstart_config/bubble.conf /etc/init/bubble.conf
-
