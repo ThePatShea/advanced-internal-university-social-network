3d4784b875ef4d915d32441784a19f3a1798e6ce
Ubuntu <ubuntu@ip-10-82-235-163.ec2.internal>
2013-08-20 21:40:32 +0000

    Added a configure script which is to be run as a normal user. The setup script and startup scripts need to be run with sudo.

--- sandbox/bubble_bundle/setup_bubble.sh
+++ sandbox/bubble_bundle/setup_bubble.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-tar -xvzf bubble_bundle.tgz
-cp start_bubble.sh ./bundle/
-cd bundle/server/
-npm install fibers@1.0.0
-cd ../../
 cp ./nginx_config/bubble /etc/nginx/sites-available/bubble
 cp ./nginx_config/nginx.conf /etc/nginx/nginx.conf
 ln -s /etc/nginx/sites-available/bubble /etc/nginx/sites-enabled/bubble
