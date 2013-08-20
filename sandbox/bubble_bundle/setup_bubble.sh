bfeb10a7cd1d54464dc855f2f5839567e5e85565
Xavier <fernandes.praphat@gmail.com>
2013-08-20 20:50:40 +0000

    Updated the Bubble Bundle setup and startup scripts.

--- sandbox/bubble_bundle/setup_bubble.sh
+++ sandbox/bubble_bundle/setup_bubble.sh
@@ -5,4 +5,7 @@ cp start_bubble.sh ./bundle/
 cd bundle/server/
 npm install fibers@1.0.0
 cd ../../
+cp ./nginx_config/bubble /etc/nginx/sites-available/bubble
+ln -s /etc/nginx/sites-available/bubble /etc/nginx/sites-enabled/bubble
+cp ./upstart_config/bubble.conf /etc/init/bubble.conf
 
