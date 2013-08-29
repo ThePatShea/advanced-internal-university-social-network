223d2ab9b5417f47454a6c57c70977e238b4568f
fernandes.praphat@gmail.com <fernandes.praphat@gmail.com>
2013-08-29 16:27:31 -0400

    THe Bubble members page now loads applicants/admins/members/invitees with virtual pagination.

--- sandbox/bubble_meteor/client/views/bubbles/bubble_members/bubble_members_page/bubble_members_page.js
+++ sandbox/bubble_meteor/client/views/bubbles/bubble_members/bubble_members_page/bubble_members_page.js
@@ -5,10 +5,15 @@ Template.bubbleMembersPage.rendered = function () {
 	var currentBubble = Bubbles.findOne({_id: currentBubbleId});
 	var adminIds = currentBubble.users.admins;
 	var memberIds = currentBubble.users.members;
-	var userIds = adminIds.concat(memberIds);
-	Meteor.subscribe('findUsersById', userIds);
+	//var userIds = adminIds.concat(memberIds);
+	//Meteor.subscribe('findUsersById', userIds);
+	var applicantIds = currentBubble.users.applicants;
+	var inviteeIds = currentBubble.users.invitees;
+	var userIds = applicantIds.concat(adminIds, memberIds, inviteeIds);
+	Meteor.subscribe('findUsersById', userIds.slice(0, 10));
 
 	var userIdList = Session.get("selectedUserIdList");
+
 	var userIdArray = [];
 	_.each(userIdList, function(userId)
 	{
@@ -32,6 +37,25 @@ Template.bubbleMembersPage.rendered = function () {
 		userIdArray.push(userId);
 	});
 	Session.set("selectedUserIdList",userIdArray);
+
+	//var numIds = 10;
+
+	$("#main").scroll(function(){
+	    //if ( ($("#main").scrollTop() >= $("#main")[0].scrollHeight - $("#main").height()) ) {
+	    	var page = Math.round( $("#main").scrollTop()/$("#main").height() );
+	    	console.log('Pre paginating: ', page, $("#main").scrollTop(), $("#main").height(), $(document).height());
+	    if ( page > 0 ){
+	      //alert(page);
+	      console.log('Scrolling: ', page, userIds.slice((page-1)*10, page*10));
+	      numIds = page*10;
+	      if(numIds >= userIds.length){
+	      	numIds = userIds.length - 1;
+	      }
+	      var pageUserIds = userIds.slice((page-1)*10, page*10);
+	      Meteor.subscribe('findUsersById', pageUserIds);
+	    }
+	  });
+
 };
 
 Template.bubbleMembersPage.helpers({
