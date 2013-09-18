2e574dffbe6f186f86e44bc42027ab94d8ca8908
Taggart Bowen-Gaddy <taggartbg-rails@gmail.com>
2013-09-18 13:36:34 -0400

    Backbone Explore back button glitch fixed

--- sandbox/bubble_meteor/client/views/explore/explore_page/explore_home/explore_home_page_backbone.js
+++ sandbox/bubble_meteor/client/views/explore/explore_page/explore_home/explore_home_page_backbone.js
@@ -1,24 +1,42 @@
 Template.explorePageBackbone.created = function(){
-	es = undefined;
-}
-
-Template.explorePageBackbone.rendered = function(){
 	//var currentExploreId = window.location.pathname.split("/")[2];
-	console.log("RENDERED!");
+	console.log("URL: ", window.location.pathname);
+	es = undefined;
+	Session.set("ExploreHome", "render");
 	currentExploreId = 'hKPb7ZohcmJEFR78W';
 	es = new BubbleData.ExploreSection({
 		exploreId: currentExploreId,
 		limit: 10,
 		fields: ['name', 'author', 'postAsType', 'postAsId', 'submitted', 'postType', 'exploreId', 'dateTime']
 	});
+	exploreDep = new Deps.Dependency;
+	es.explorePosts.on("change", function() {
+		console.log("explore posts changed");
+		exploreDep.changed();
+	})
+	es.exploreBubbles.on("change", function() {
+		console.log("explore bubbles changed");
+		exploreDep.changed();
+	});
+	es.exploreUsers.on("change", function() {
+		console.log("explore users changed");
+		exploreDep.changed();
+	});
+}
+
+Template.explorePageBackbone.rendered = function(){
+	console.log("RENDERED!");
+	//es.getPage(0);
 }
 
 Template.explorePageBackbone.helpers({ 
   posts: function(){
+  	exploreDep.depend();
   	if(es != undefined)
 	{
 	  	retVal = es.explorePosts.toJSON();
 	  	bubbles = es.exploreBubbles.toJSON();
+	  	console.log("Bubble JSON: ", bubbles);
 	  	users = es.exploreUsers.toJSON();
 	  	_.each(retVal, function(post, i) {
 	  		if(post.postAsType === "bubble")
