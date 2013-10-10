605d6c2dd138f9fc69febff1a13771b08968de69
Taggart Bowen-Gaddy <taggartbg-rails@gmail.com>
2013-10-10 17:49:53 -0400

    Updates on mybubbles working

--- sandbox/bubble_meteor/client/views/bubbles/bubble_page/bubble_home/bubble_home_page_backbone.js
+++ sandbox/bubble_meteor/client/views/bubbles/bubble_page/bubble_home/bubble_home_page_backbone.js
@@ -2,6 +2,7 @@
 referenceDateTime = moment().add('hours',-4).valueOf();
 
 Template.bubblePageBackbone.created = function() {
+  Session.set('updatesToShow',3);
 
   bubbleDep = new Deps.Dependency;
   if(typeof goingDep === "undefined")
@@ -138,7 +139,146 @@ Template.bubblePageBackbone.rendered = function() {
 
 }
 
-Template.bubblePageBackbone.helpers({ 
+Template.bubblePageBackbone.helpers({
+    testNumUpdates: function() {
+    if(Session.get('testNumUpdates') > 0)
+      return Session.get('testNumUpdates');
+    else
+      return 0;
+  },
+
+  testGetUpdates: function() {
+    var updateList = Updates.find({userId: Meteor.userId(), bubbleId: window.location.pathname.split("/")[2], read:false}).fetch();
+
+    if(updateList.length > 0) {
+        //To combine updates with same userId, invokerId, updateType and postId
+        _.each(updateList, function(update){
+          updateList = _.reject(updateList, function(newUpdate) {
+            return  update.bubbleId == newUpdate.bubbleId && 
+                    update.userId == newUpdate.userId && 
+                    update.invokerId == newUpdate.invokerId && 
+                    update.updateType == newUpdate.updateType &&
+                    update.postId == newUpdate.postId;
+          });
+          if(!_.contains(updateList,update)){
+            updateList.push(update);
+          }
+        });
+
+        /**
+        * To combine updates for comments in the same post
+        **/
+        _.each(updateList, function(update){
+
+          var commentUpdates = _.reject(updateList, function(update) {
+            return update.updateType != "replied";
+          });
+
+          //Combine and chain the names together
+          if (commentUpdates.length > 0) {
+            updateList = _.reject(updateList, function(newUpdate) {
+              return update.postId == newUpdate.postId && 
+                      update.updateType == newUpdate.updateType &&
+                      update.updateType == "replied";
+            });
+            if(!_.contains(updateList,update)) {
+              //Pull out comment updates that belong to the same post
+              singleTypeUpdates = _.reject(commentUpdates, function(newUpdate) {
+                return update.postId != newUpdate.postId;
+              });
+              if (singleTypeUpdates.length > 0) {
+                //Create the chained name
+                var nameArray = _.pluck(singleTypeUpdates,"invokerName");
+                var chainedName = nameArray.join();
+                var maxLength = 13;
+
+                //Checks to see if the length of names exceed a certain limit
+                if(chainedName.length > maxLength) {
+                  chainedName = chainedName.substring(0,maxLength);
+                  var nameList = chainedName.split(',');
+                  if(nameArray[0].length > maxLength) {
+                    nameList[0] = nameArray[0];
+                  }else{
+                    nameList.pop();
+                  }
+                  var excessCount = nameArray.length - nameList.length;
+                  chainedName = nameList.join();
+                  if(excessCount == 1) {
+                    chainedName = chainedName + " and " + excessCount + " other";
+                  }else if(excessCount > 1){
+                    chainedName = chainedName + " and " + excessCount + " others";
+                  }
+                }else{
+                  chainedName = chainedName.replace(/,([^,]*)$/," and $1");
+                }
+
+                //Add the chained name to the invokerName
+                update.invokerName = chainedName;
+              }
+              updateList.push(update);
+            }
+          }
+        });
+
+        /**
+        *  To combine and chain up names for similar updates
+        **/
+        _.each(updateList, function(originalUpdate) {
+          if(originalUpdate.collapsible == true){
+            var type = originalUpdate.updateType;
+            var singleTypeUpdates = _.reject(updateList, function(update) {
+              return update.updateType != type;
+            });
+            if (singleTypeUpdates.length > 0) {
+              var nameArray = _.pluck(singleTypeUpdates,"invokerName");
+              var chainedName = nameArray.join();
+              var maxLength = 13;
+
+              if(chainedName.length > maxLength) {
+                chainedName = chainedName.substring(0,maxLength);
+                var nameList = chainedName.split(',');
+                if(nameArray[0].length > maxLength) {
+                  nameList[0] = nameArray[0];
+                }else{
+                  nameList.pop();
+                }
+                var excessCount = nameArray.length - nameList.length;
+                chainedName = nameList.join();
+                if(excessCount == 1) {
+                  chainedName = chainedName + " and " + excessCount + " other";
+                }else{
+                  chainedName = chainedName + " and " + excessCount + " others";
+                } 
+              }else{
+                chainedName = chainedName.replace(/,([^,]*)$/," and $1");
+              }
+
+              originalUpdate.invokerName = chainedName;
+              // Next remove all applicants
+              updateList = _.reject(updateList, function(newUpdate) {
+                return newUpdate.updateType == type;
+              });
+              //Now add back with the applicant that has a changed invoker name
+              if(firstUpdate){
+                updateList.push(firstUpdate);
+              }
+            }
+          }
+        });
+
+        updateList = _.sortBy(updateList, function(newUpdate) {
+          return newUpdate.submitted; 
+        });  
+        
+      Session.set("testNumUpdates",updateList.length);
+
+        if(Session.get('updatesToShow')>0){
+          return _.first(updateList.reverse(), Session.get('updatesToShow'));
+        }else{
+          return updateList.reverse();
+        }
+    }
+  },
   getCurrentBubbleBackbone: function(){
     var bubble = mybubbles.bubbleInfo.toJSON();
     return bubble;
@@ -255,11 +395,11 @@ Template.bubblePageBackbone.events({
     var updates = Updates.find({bubbleId: currentBubbleId, userId: Meteor.userId(), read:false}).fetch();
     _.each(updates, function(update) {
       Meteor.call('setRead', update);
+      Session.set('testNumUpdates',0);
     });
   },
 
   'click .more-updates': function() {
-    Session.set('numUpdates', 0);
-    console.log(Session.get('numUpdates'));
+    Session.set('updatesToShow', 0);
   }
 });
