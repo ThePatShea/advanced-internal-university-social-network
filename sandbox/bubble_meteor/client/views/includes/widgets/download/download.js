aa8ca09ed8a2d5fcee5e5113e74438092f3958a7
fernandes.praphat@gmail.com <fernandes.praphat@gmail.com>
2013-10-21 14:46:56 -0400

    File download now works in the list of posts in MyBubbles. The compromise solution opens the file in a new tab.

--- sandbox/bubble_meteor/client/views/includes/widgets/download/download.js
+++ sandbox/bubble_meteor/client/views/includes/widgets/download/download.js
@@ -2,18 +2,32 @@ Template.download.events({
     'click .download': function(event) {
       // Disable the parent button
         event.stopPropagation();
+        event.preventDefault();
+
+        if(typeof this.id == 'undefined'){
+          this.id = this._id;
+        }
+
+        currentPostObject = new mybubbles.BubblePost({id: this.id});
+        currentPostObject.fetch({async: false});
+        currentPost = currentPostObject.toJSON();
+        console.log('Download: ', currentPost);
 
       // Track action on Google Analytics
         _gaq.push(['_trackEvent', 'File', 'Download', this.name]);
 
       // Add to the numDownloads count
-        Posts.update(this._id, {$set: {lastDownloadTime: new Date().getTime()}, $inc: {numDownloads: 1} }, function(error) {
+        Posts.update({_id: this.id}, {$set: {lastDownloadTime: new Date().getTime()}, $inc: {numDownloads: 1} }, function(error) {
           if (error) {
             // display the error to the user
             throwError(error.reason);
+            console.log('Error updating post: ', error);
           } else {
-            Meteor.Router.to('postPage', currentPostId);
+            //Meteor.Router.to('postPage', currentPostId);
+            console.log('Updating post download time');
           }
         });
+
+        window.open(currentPost.file,'_.blank');
     }
 });
