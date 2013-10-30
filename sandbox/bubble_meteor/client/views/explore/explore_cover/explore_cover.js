7ffdd8b505f57c6abdd50e461786186cfd963559
Taggart Bowen-Gaddy <taggartbg-rails@gmail.com>
2013-10-30 11:41:22 -0400

    Removed the post to explore section button unless youre on the explore home page

--- sandbox/bubble_meteor/client/views/explore/explore_cover/explore_cover.js
+++ sandbox/bubble_meteor/client/views/explore/explore_cover/explore_cover.js
@@ -1,4 +1,15 @@
 Template.exploreCover.helpers({
+  onExploreHome: function(){
+    if(window.location.pathname.split("/")[3] == 'home')
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  },
+
   getExploreIcon: function(exploreObject){
       var iconName = exploreObject.exploreIcon;
       if(iconName == 'announcements'){
