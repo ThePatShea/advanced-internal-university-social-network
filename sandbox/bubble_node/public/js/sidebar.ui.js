b754512175c75e32077b4c70fa969a93f457b413
Joe <ussefo@gmail.com>
2013-10-31 06:27:40 +0000

    Drop down menu(notifcation) done with animations and sub classes/options bar hover effects, Now using CSS3 arrows instead of svg arrows so code will be more cleaner, I don't suggest getting near the css file or your head will explore i still have to clean it up and comment things out, Also fonts will look weird and fuzzy but as soon as typekit is linked probably thing should look like it's a copy paste of the mockups, More updates coming when i wake up

--- sandbox/bubble_node/public/js/sidebar.ui.js
+++ sandbox/bubble_node/public/js/sidebar.ui.js
@@ -4,4 +4,10 @@ $(document).ready(function(){
 	  $('.side-bar button').removeClass('active');
 	  $(this).addClass('active');
 	});
+
+	
+
+	// $('.hasItems').click(function(){
+	//   $('.drop').addClass('drop-active');
+	// });
 });
