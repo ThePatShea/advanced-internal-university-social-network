893335b74c941435e2d26f00afb06ef2716a3af5
fernandes.praphat@gmail.com <fernandes.praphat@gmail.com>
2013-11-04 13:08:24 -0500

    Added subpanel all to the main Marionette app.

--- sandbox/bubble_node/client/app.js
+++ sandbox/bubble_node/client/app.js
@@ -4,7 +4,8 @@ $(document).ready(function(){
 
 	app.addRegions({
 		header: '#header_container',
-		sidebar: '#sidebar_container'
+		sidebar: '#sidebar_container',
+		subpanel: '#subpanel_container'
 	});
 
 	TopBarView = Backbone.Marionette.ItemView.extend({
@@ -15,10 +16,16 @@ $(document).ready(function(){
 		template: Templates['./client/apps/sidebar/templates/sidebar.html.handlebars']
 	});
 
+	SubpanelView = Backbone.Marionette.ItemView.extend({
+		template: Templates['./client/apps/subpanel/templates/subpanel.html.handlebars']
+	});
+
 	var view1 = new TopBarView();
 	var view2 = new SidebarView();
+	var view3 = new SubpanelView();
 
 	app.header.show(view1);
 	app.sidebar.show(view2);
+	app.subpanel.show(view3);
 	console.log('App running');
 });
