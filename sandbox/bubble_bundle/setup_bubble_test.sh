8d0e5896129549854b647a55a6ee7e6be0720899
fernandes.praphat@gmail.com <fernandes.praphat@gmail.com>
2013-11-08 15:09:29 -0500

    Updated the snapshot and deploy scripts.

---
+++ sandbox/bubble_bundle/setup_bubble_test.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cp ./nginx_config/bubble.test /etc/nginx/sites-available/bubble
+cp ./nginx_config/nginx.conf.test /etc/nginx/nginx.conf
+ln -s /etc/nginx/sites-available/bubble /etc/nginx/sites-enabled/bubble
+service nginx restart
+cp ./upstart_config/bubble.conf /etc/init/bubble.conf
