6a53244ccddff5249aa5104a896b97562d81cc27
fernandes.praphat@gmail.com <fernandes.praphat@gmail.com>
2013-11-08 15:31:54 -0500

    Added a configure_test script and fixed more bugs in fab file.

--- sandbox/bubble_bundle/ec2_scripts/fabfile.py
+++ sandbox/bubble_bundle/ec2_scripts/fabfile.py
@@ -66,6 +66,52 @@ def create_bundle_image(blank_ami_id, path_to_bundle, new_ami_name, secure_flag,
 
 
 
+def create_bundle_test_image(blank_ami_id, path_to_bundle, new_ami_name, secure_flag, git_login, git_password):
+	"""
+	Creates an EC2 instance, installs the Emory Bubble Test Bundle on it. And takes a snapshot to create an AMI
+	"""
+
+	print (_green("Started..."))
+
+	ec2 = boto.connect_ec2()
+	reservation = ec2.run_instances(image_id=blank_ami_id, key_name='xavier-macbook-campusbubble', security_groups=['WEB'], instance_type='m1.large', placement='us-east-1b')
+	print reservation
+	time.sleep(10)
+	instance = reservation.instances[0]
+	instance.update()
+	while instance.state == u'pending':
+		print (_yellow("Instance state: %s" % instance.state))
+		time.sleep(10)
+		instance.update()
+
+	print (_green("Instance state: %s" % instance.state))
+	print (_green("Instance public DNS name: %s" % instance.public_dns_name))
+
+	time.sleep(30)
+
+	execute(deploy_bundle_test, path_to_bundle, git_login, git_password, hosts=['ubuntu@'+instance.public_dns_name])
+
+	print(_green("Deployed bundle..."))
+
+	
+	new_ami_id = ec2.create_image(instance.id, new_ami_name)
+	print (_yellow("New AMI created: %s" % new_ami_id))
+
+	images = ec2.get_all_images(image_ids=[new_ami_id])
+	new_image = images[0]
+	while new_image.state != u'available':
+		print (_yellow("New AMI is %s" % new_image.state))
+		time.sleep(10)
+		images = ec2.get_all_images(image_ids=[new_ami_id])
+		new_image = images[0]
+
+	print (_green("New AMI %s is available" % new_ami_id))
+
+	ec2.terminate_instances(instance_ids=[instance.id])
+
+
+
+
 def start_bundle_instance(blank_ami_id, new_instance_name, path_to_bundle, secure_flag, git_login, git_password):
 	"""
 	Starts up a new mi.large instance with the bundle deployed to it and running
@@ -157,7 +203,7 @@ def deploy_bundle_test(path_to_bundle, git_login, git_password):
 	run('sudo npm install -g fibers@1.0.0')
 	with cd('/home/ubuntu/emory_bubble/bubble-3/sandbox/bubble_bundle'):
 		#run('git pull')
-		run('sudo stop bubble')
+		#run('sudo stop bubble')
 		run('./configure_test.sh')
 		run('sudo ./setup_bubble_test.sh')
 		run('sudo ./start_bubble_test.sh')
