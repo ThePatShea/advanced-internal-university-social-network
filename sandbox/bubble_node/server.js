a2615bb7ba0fc8d5cb32af664a77efa7167d2632
Loren Norman <lorennorman@gmail.com>
2013-11-11 14:50:57 -0500

    clean up the server side files

--- sandbox/bubble_node/server.js
+++ sandbox/bubble_node/server.js
@@ -1,56 +1,57 @@
-var _ = require("underscore");
-var express = require('express');
-var app = express();
-var stylus = require('stylus');
-var path = require('path');
-var handlebars = require('hbs');
-var Snockets = require('snockets');
-var snockets = new Snockets();
+var _          = require("underscore")
+  , express    = require('express')
+  , app        = express()
+  , stylus     = require('stylus')
+  , path       = require('path')
+  , handlebars = require('hbs')
+  , Snockets   = require('snockets')
+  , snockets   = new Snockets();
 
-//Tell Express to use the Stylus middleware to 
-//compile any .styl files under the public folder
+
+// Set location of static content, compile Stylus files there
+app.use(express.static(__dirname + '/server/public'));
 app.use(stylus.middleware(path.resolve(__dirname, '/server/public')));
 
-//Set Handlebars as the templating engine on files ending in .html.handlebars
+// Serve up the backbone app files raw, but only in development
+app.configure('development', function() {
+  app.use('/client', express.static(__dirname + '/client'));
+});
+
+// Set location of Express views with Handlebars as the template language
+app.set('views', __dirname + '/server/views');
 app.set('view engine', '.html.handlebars');
 app.engine('.html.handlebars', handlebars.__express);
 
-//Register Handlebars helpers
+// Handlebars asset pipeline helper
 handlebars.registerHelper("js", function(){
-  js = snockets.getCompiledChain('client/manifest.js', {async: false});
 
-  scripts = _(js).map(function(jsFile) {
-    return "<script src='" + jsFile.filename + "'></script>";
-  }).join("\n  ");
+  if(process.env.NODE_ENV != 'production') {
+    // Output a dependency-ordered series of <script> tags
+    js = snockets.getCompiledChain('client/manifest.js', { async: false });
+
+    scripts = _(js).map(function(jsFile) {
+      return "<script src='" + jsFile.filename + "'></script>";
+    }).join("\n  ");
+  } else {
+    // Output the <script> tag of the compiled application (see its route below)
+    scripts = "<script src='application.js'></script>";
+  }
 
   return scripts;
 });
 
-handlebars.registerHelper("CSSAssets", function(){
-  return '/css/style.css';
-});
 
-//var environment = process.env.NODE_ENV;
-
-handlebars.registerHelper("isProduction", function(environment){
-  return false;
-});
-
-//Set location of static content
-app.use(express.static(__dirname + '/server/public'));
-
-//Set location of Backbone Marionette app static files
-app.use('/client', express.static(__dirname + '/client'));
-
-//Set location of Express views
-app.set('views', __dirname + '/server/views');
-
-
-//Server Routes
+// Server Routes
 app.get('/', function(req, res) {
   res.render('app'); // The main app template
 });
 
+// The compiled and minifed Marionette application
+app.get('/application.js', function(req, res) {
+  minifiedApp = snockets.getConcatenation('client/manifest.js', { async: false, minify: true });
+  res.type('application/javascript');
+  res.send(minifiedApp);
+});
 
 
 console.log('Server listening on 3000');
