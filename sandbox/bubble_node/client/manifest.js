0c2b53537417247fa577f356a668d2e3e472f1d9
Loren Norman <lorennorman@gmail.com>
2013-11-11 14:06:38 -0500

    Build a real asset pipeline into the app with Snockets

---
+++ sandbox/bubble_node/client/manifest.js
@@ -0,0 +1,5 @@
+//= require app
+//= require_tree utilities
+//= require_tree controllers
+//= require_tree models
+//= require_tree apps
--- sandbox/bubble_node/server.js
+++ sandbox/bubble_node/server.js
@@ -1,30 +1,38 @@
+var _ = require("underscore");
 var express = require('express');
 var app = express();
 var stylus = require('stylus');
 var path = require('path');
-var hbs = require('hbs');
+var handlebars = require('hbs');
+var Snockets = require('snockets');
+var snockets = new Snockets();
 
 //Tell Express to use the Stylus middleware to 
 //compile any .styl files under the public folder
 app.use(stylus.middleware(path.resolve(__dirname, '/server/public')));
 
-//Set Handlebars as the templating engine on files ending in handlebars.html
-//app.set('view engine', 'hbs');
-app.set('view engine', 'html.handlebars');
-app.engine('html.handlebars', require('hbs').__express);
+//Set Handlebars as the templating engine on files ending in .html.handlebars
+app.set('view engine', '.html.handlebars');
+app.engine('.html.handlebars', handlebars.__express);
 
 //Register Handlebars helpers
-hbs.registerHelper("JavaScriptAssets", function(){
-  return '/js/compiled.js';
+handlebars.registerHelper("js", function(){
+  js = snockets.getCompiledChain('client/manifest.js', {async: false});
+
+  scripts = _(js).map(function(jsFile) {
+    return "<script src='" + jsFile.filename + "'></script>";
+  }).join("\n  ");
+
+  return scripts;
 });
 
-hbs.registerHelper("CSSAssets", function(){
+handlebars.registerHelper("CSSAssets", function(){
   return '/css/style.css';
 });
 
 //var environment = process.env.NODE_ENV;
 
-hbs.registerHelper("isProduction", function(environment){
+handlebars.registerHelper("isProduction", function(environment){
   return false;
 });
 
@@ -32,21 +40,16 @@ hbs.registerHelper("isProduction", function(environment){
 app.use(express.static(__dirname + '/server/public'));
 
 //Set location of Backbone Marionette app static files
-app.use('/app', express.static(__dirname + '/client'));
+app.use('/client', express.static(__dirname + '/client'));
 
 //Set location of Express views
 app.set('views', __dirname + '/server/views');
 
 
 //Server Routes
-app.get('/server', function(req, res){
-	res.render('index');
+app.get('/', function(req, res) {
+  res.render('app'); // The main app template
 });
-
-app.get('/currentdate', function(req, res){
-	res.render('currentdatetemplate', {'date': Date()});
-});
-
 
 
 
