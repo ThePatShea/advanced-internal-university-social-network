ed7488a870aef9941c8b042537e79ee8e0c44626
Loren Norman <lorennorman@gmail.com>
2013-11-15 20:39:01 -0500

    Add multiple template lookup paths to support components

--- sandbox/bubble_node/client/utilities/get_template.js
+++ sandbox/bubble_node/client/utilities/get_template.js
@@ -1,12 +1,22 @@
 App.getTemplate = function(templateLookup, data) {
   data = data || {};
 
-  templatePath = './client/apps/' + templateLookup + '.html.handlebars'
-  return Templates[templatePath](data);
-}
+  lookupRoots = ["./client/apps", "./client/components"];
+
+  var lookups = _(lookupRoots).map(function(root) {
+    return root + "/" + templateLookup + ".html.handlebars";
+  });
+
+  // Find the first one that exists
+  var path = _.find(lookups, function(lookup){
+    return Templates[lookup]
+  });
+
+  return Templates[path](data);
+};
 
 _.extend(Marionette.Renderer, {
   render: function(template, data) {
     return App.getTemplate(template, data);
   }
-})
+});
