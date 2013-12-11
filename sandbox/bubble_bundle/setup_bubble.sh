c9083fde5720b5117a20e309b598a1f5df9f9c6b
fernandes.praphat@gmail.com <fernandes.praphat@gmail.com>
2013-12-11 15:35:56 -0500

    Configure for deployment against Object Rocket

--- sandbox/bubble_bundle/setup_bubble.sh
+++ sandbox/bubble_bundle/setup_bubble.sh
@@ -4,4 +4,4 @@ cp ./nginx_config/bubble /etc/nginx/sites-available/bubble
 cp ./nginx_config/nginx.conf /etc/nginx/nginx.conf
 ln -s /etc/nginx/sites-available/bubble /etc/nginx/sites-enabled/bubble
 service nginx restart
-cp ./upstart_config/bubble.conf /etc/init/bubble.conf
+cp ./upstart_config/objectrocket/bubble.conf /etc/init/bubble.conf
