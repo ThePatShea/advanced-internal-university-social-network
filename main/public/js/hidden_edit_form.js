b2b7a07532850af1788b835bc3f9685d6fc3d567
Pat Shea <patshea56@gmail.com>
2015-05-13 03:20:01 -0400

    updated files

--- main/public/js/hidden_edit_form.js
+++
@@ -1,8 +0,0 @@
-// Keeps the hidden edit form in sync witht he data the user inputs
-$(document).ready(function () {
-
-  $('.connect_hidden').change(function() {
-    $( '#hidden_' + $(this).attr('id') ).val( $(this).val() )
-  });
-
-});
